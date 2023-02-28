98aed2b0400c8bc255a49c0e553fa34305bc2f09
Coding for fun <peter@reuteras.net>
2023-02-28 12:08:08 +0100

    Remove files that are not needed.

--- setup/installing.ps1
+++
@@ -1,4 +0,0 @@
-# Installing tools
-PowerShell.exe -ExecutionPolicy Bypass -File C:\Users\WDAGUtilityAccount\Documents\tools\Update-Wallpaper.ps1 C:\Users\WDAGUtilityAccount\Documents\tools\downloads\installing.png
-$shell = New-Object -ComObject "Shell.Application"
-$shell.sendkeys('{F5}')
