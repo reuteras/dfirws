07a3f5b1075d8c28c4e9d9db03483a2149079f42
Coding for fun <peter@reuteras.net>
2023-02-28 13:21:27 +0100

    Update for new directory structure.

--- createSandboxConfig.ps1
+++ createSandboxConfig.ps1
@@ -1,7 +1,7 @@
 (Get-Content sandbox_clean_no_network.wsb.template).replace('__SANDBOX__', $PSScriptRoot) | Set-Content .\sandbox_clean_no_network.wsb
 (Get-Content sandbox_tools_no_network.wsb.template).replace('__SANDBOX__', $PSScriptRoot) | Set-Content .\sandbox_tools_no_network.wsb
 
-if (! (Test-Path -Path .\tools\config.txt)) {
-    Copy-Item .\tools\default-config.txt .\tools\config.txt
+if (! (Test-Path -Path .\setup\config.txt)) {
+    Copy-Item .\setup\default-config.txt .\setup\config.txt
     Write-Output "Created .\tools\config.txt. Select tools to install based on your needs."
 }
