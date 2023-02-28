d0cc74ffc481f0ed1d466853eeea5918a409434c
Coding for fun <peter@reuteras.net>
2023-02-28 13:20:08 +0100

    Update for new directory structure.

--- downloadFiles.ps1
+++ downloadFiles.ps1
@@ -1,8 +1,17 @@
-New-Item -ItemType Directory -Force -Path .\tools\downloads > $null
+# Download and update files
 
+if ( tasklist | findstr Sandbox ) {
+    Write-Host "Sandbox can't be running during upgrade."
+    Exit
+}
+
+New-Item -ItemType Directory -Force -Path .\downloads > $null 2>&1
+
+# Cleanup
 if (Test-Path -Path .\log\log.txt) {
     Remove-Item .\log\log.txt
 }
+Remove-Item -Recurse -Force .\tmp\downloads\ > $null 2>&1
 
 .\resources\download\didier.ps1
 .\resources\download\git.ps1
@@ -10,10 +19,9 @@ if (Test-Path -Path .\log\log.txt) {
 .\resources\download\python.ps1
 .\resources\download\release.ps1
 .\resources\download\zimmerman.ps1
+.\resources\download\unpack.ps1
 Write-Output "Copy files"
-Copy-Item README.md .\tools\downloads\
-Copy-Item .\resources\images\copying.png .\tools\downloads\
-Copy-Item .\resources\images\installing.png .\tools\downloads\
-Copy-Item .\resources\images\python.png .\tools\downloads\
-Copy-Item .\resources\images\sans.jpg .\tools\downloads\
+Copy-Item README.md .\downloads\
+Copy-Item .\resources\images\sans.jpg .\downloads\
+Write-Output "" > .\downloads\done.txt
 Write-Output "Download done"
