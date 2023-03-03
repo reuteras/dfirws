4edf79ac328b63e197fc8c16e90236a82b37d659
Coding for fun <peter@reuteras.net>
2023-03-03 13:29:34 +0100

    Add example script to customize the sandbox.

---
+++ local/example-customize.ps1
@@ -0,0 +1,5 @@
+# Rename to customize.ps1 to use.
+# Add files to this folder and they will be available as C:\local
+
+# Customize background image:
+# PowerShell.exe -ExecutionPolicy Bypass -File C:\Users\WDAGUtilityAccount\Documents\tools\Update-Wallpaper.ps1 C:\local\image.png
