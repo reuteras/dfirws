c800ef3160cf4be2e29a8bd87adde141eabc79be
Coding for fun <peter@reuteras.net>
2023-03-05 13:49:11 +0100

    Reorder code and enable more customization.

--- setup/helpers.ps1
+++ setup/helpers.ps1
@@ -1,3 +1,5 @@
+Write-Output "Start helpers.ps1"
+
 # Declare helper functions
 function Add-ToUserPath {
     param (
@@ -13,7 +15,7 @@ function Add-ToUserPath {
     if (!($path.Contains($dir))) {
         # append dir to path
         [Environment]::SetEnvironmentVariable("PATH", $path + ";$dir", [EnvironmentVariableTarget]::User)
-        Write-Host "Added $dir to PATH"
+        Write-Output "Added $dir to PATH"
         return
     }
     Write-Error "$dir is already in PATH"
@@ -37,6 +39,16 @@ Set-ItemProperty -Path "HKCU:\Control Panel\International" -name sLongDate -valu
 Set-ItemProperty -Path "HKCU:\Control Panel\International" -name sShortTime -value "HH:mm"
 Set-ItemProperty -Path "HKCU:\Control Panel\International" -name sTimeFormat -value "HH:mm:ss"
 
+Write-Output "Show file extensions"
+REG ADD HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced /v HideFileExt /t REG_DWORD /d 0 /f
+REG ADD HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced\Folder\HideFileExt /v DefaultValue /t REG_DWORD /d 0 /f
+REG ADD HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced\Folder\Hidden\SHOWALL /v DefaultValue /t REG_DWORD /d 1 /f
+REG ADD HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced /v HideFileExt /t REG_DWORD /d 0 /f
+REG ADD HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced /v Hidden /t REG_DWORD /d 0 /f
+REG ADD HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced /v ShowSuperHidden /t REG_DWORD /d 1 /f
+REG ADD HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced /v DontPrettyPath /t REG_DWORD /d 1 /f
+Stop-Process -ProcessName Explorer -Force
+
 Write-Output "Add to PATH"
 Add-ToUserPath "C:\Program Files\7-Zip"
 Add-ToUserPath "C:\Program Files\Git\bin"
@@ -52,7 +64,6 @@ Add-ToUserPath "C:\Tools\fakenet"
 Add-ToUserPath "C:\Tools\floss"
 Add-ToUserPath "C:\Tools\FullEventLogView"
 Add-ToUserPath "C:\Tools\GoReSym"
-Add-ToUserPath "C:\Tools\loki"
 Add-ToUserPath "C:\Tools\malcat\bin"
 Add-ToUserPath "C:\Tools\nmap"
 Add-ToUserPath "C:\Tools\qpdf\bin"
@@ -85,12 +96,18 @@ REG ADD "HKU\%1\Software\Microsoft\Windows\CurrentVersion\Explorer" /v "link" /t
 if ( $env:WSDFIR_GIT -eq '"Yes"' ) {
     Set-Shortcut "C:\Users\WDAGUtilityAccount\Desktop\bash.lnk" "C:\Program Files\Git\bin\bash.exe" "C:\Users\WDAGUtilityAccount\Desktop"
 }
-Set-Shortcut "C:\Users\WDAGUtilityAccount\Desktop\CyberChef.lnk" "C:\Tools\CyberChef\CyberChef.html"
+if ( $env:WSDFIR_CYBERCHEF -eq '"Yes"' ) {
+    Set-Shortcut "C:\Users\WDAGUtilityAccount\Desktop\CyberChef.lnk" "C:\Tools\CyberChef\CyberChef.html"
+}
 if ( $env:WSDFIR_CMDER -eq '"Yes"' ) {
     Set-Shortcut "C:\Users\WDAGUtilityAccount\Desktop\cmder.lnk" "C:\Tools\cmder\cmder.exe" "C:\Users\WDAGUtilityAccount\Desktop"
 }
-Set-Shortcut "C:\Users\WDAGUtilityAccount\Desktop\Cutter.lnk" "C:\Tools\cutter\cutter.exe"
-Set-Shortcut "C:\Users\WDAGUtilityAccount\Desktop\dnSpy.lnk" "C:\Tools\dnSpy\dnSpy.exe"
+if ( $env:WSDFIR_CUTTER -eq '"Yes"' ) {
+    Set-Shortcut "C:\Users\WDAGUtilityAccount\Desktop\Cutter.lnk" "C:\Tools\cutter\cutter.exe"
+}
+if ( $env:WSDFIR_DNSPY -eq '"Yes"' ) {
+    Set-Shortcut "C:\Users\WDAGUtilityAccount\Desktop\dnSpy.lnk" "C:\Tools\dnSpy\dnSpy.exe"
+}
 if ( $env:WSDFIR_FLV -eq '"Yes"') {
     Set-Shortcut "C:\Users\WDAGUtilityAccount\Desktop\FullEventLogView.lnk" "C:\Tools\FullEventLogView\FullEventLogView.exe"
 }
@@ -100,32 +117,33 @@ if ( $env:WSDFIR_JAVA -eq '"Yes"' ) {
 if ( $env:WSDFIR_HXD -eq '"Yes"' ) {
     Set-Shortcut "C:\Users\WDAGUtilityAccount\Desktop\HxD.lnk" "C:\Program Files\HxD\HxD.exe"
 }
-Set-Shortcut "C:\Users\WDAGUtilityAccount\Desktop\Malcat.lnk" "C:\Tools\Malcat\bin\malcat.exe"
+if ( $env:WSDFIR_MALCAT -eq '"Yes"' ) {
+    Set-Shortcut "C:\Users\WDAGUtilityAccount\Desktop\Malcat.lnk" "C:\Tools\Malcat\bin\malcat.exe"
+}
 if ( $env:WSDFIR_JAVA -eq '"Yes"' ) {
     Set-Shortcut "C:\Users\WDAGUtilityAccount\Desktop\msgviewer.lnk" "C:\Tools\lib\msgviewer.jar"
 }
 if ( $env:WSDFIR_NPP -eq '"Yes"' ) {
     Set-Shortcut "C:\Users\WDAGUtilityAccount\Desktop\Notepad++.lnk" "C:\Program Files\Notepad++\notepad++.exe"
 }
-Set-Shortcut "C:\Users\WDAGUtilityAccount\Desktop\PE-bear.lnk" "C:\Tools\pebear\PE-bear.exe"
-Set-Shortcut "C:\Users\WDAGUtilityAccount\Desktop\pestudio.lnk" "C:\Tools\pestudio\pestudio\pestudio.exe"
+if ( $env:WSDFIR_PEBEAR -eq '"Yes"' ) {
+    Set-Shortcut "C:\Users\WDAGUtilityAccount\Desktop\PE-bear.lnk" "C:\Tools\pebear\PE-bear.exe"
+}
+if ( $env:WSDFIR_PESTUDIO -eq '"Yes"' ) {
+    Set-Shortcut "C:\Users\WDAGUtilityAccount\Desktop\pestudio.lnk" "C:\Tools\pestudio\pestudio\pestudio.exe"
+}
 Copy-Item "C:\Users\WDAGUtilityAccount\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Windows PowerShell\Windows PowerShell.lnk" "C:\Users\WDAGUtilityAccount\Desktop\PowerShell.lnk"
-Set-Shortcut "C:\Users\WDAGUtilityAccount\Desktop\Tools.lnk" "C:\Tools"
-Set-Shortcut "C:\Users\WDAGUtilityAccount\Desktop\x64dbg.lnk" "C:\Tools\x64dbg\release\x64\x64dbg.exe"
+if ( $env:WSDFIR_TOOLS -eq '"Yes"' ) {
+    Set-Shortcut "C:\Users\WDAGUtilityAccount\Desktop\Tools.lnk" "C:\Tools"
+}
+if ( $env:WSDFIR_X64DBG -eq '"Yes"' ) {
+    Set-Shortcut "C:\Users\WDAGUtilityAccount\Desktop\x64dbg.lnk" "C:\Tools\x64dbg\release\x64\x64dbg.exe"
+}
 
 # PowerShell
 Copy-Item "C:\Users\WDAGUtilityAccount\Documents\tools\Microsoft.PowerShell_profile.ps1" "C:\Users\WDAGUtilityAccount\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1"
 New-Item -ItemType Directory $PSHome\Modules\PSDecode
 Copy-Item "C:\Users\WDAGUtilityAccount\Documents\tools\utils\PSDecode.psm1" "$PSHome\Modules\PSDecode"
-
-Write-Output "Show file extensions"
-REG ADD HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced /v HideFileExt /t REG_DWORD /d 0 /f
-REG ADD HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced\Folder\HideFileExt /v DefaultValue /t REG_DWORD /d 0 /f
-REG ADD HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced\Folder\Hidden\SHOWALL /v DefaultValue /t REG_DWORD /d 1 /f
-REG ADD HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced /v HideFileExt /t REG_DWORD /d 0 /f
-REG ADD HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced /v Hidden /t REG_DWORD /d 0 /f
-REG ADD HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced /v ShowSuperHidden /t REG_DWORD /d 1 /f
-REG ADD HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced /v DontPrettyPath /t REG_DWORD /d 1 /f
 
 New-Item -Path HKLM:\SOFTWARE\Wow6432Node\Policies\Microsoft\Windows\PowerShell\ScriptBlockLogging -Force
 Set-ItemProperty -Path HKLM:\SOFTWARE\Wow6432Node\Policies\Microsoft\Windows\PowerShell\ScriptBlockLogging -Name EnableScriptBlockLogging -Value 1 -Force
@@ -142,10 +160,8 @@ if ( $env:WSDFIR_CMDER -eq '"Yes"' ) {
 (Get-ChildItem -File C:\venv\Scripts\).Name | findstr /R /V "[\._]" | findstr /V activate | `
     ForEach-Object {Write-Output "function $_() { python C:\venv\Scripts\$_ `$PsBoundParameters.Values + `$args }"} | Out-File -Append -Encoding "ascii" "C:\Users\WDAGUtilityAccount\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1"
 
-Stop-Process -ProcessName Explorer -Force
-
-# Signal that everything is done.
-Copy-Item "C:\Users\WDAGUtilityAccount\Documents\tools\downloads\README.md" "C:\Users\WDAGUtilityAccount\Desktop\"
+# Signal that everything is done to start using the tools (mostly).
+Copy-Item "C:\downloads\README.md" "C:\Users\WDAGUtilityAccount\Desktop\"
 PowerShell.exe -ExecutionPolicy Bypass -File C:\Users\WDAGUtilityAccount\Documents\tools\Update-Wallpaper.ps1 C:\downloads\sans.jpg
 C:\Tools\sysinternals\Bginfo64.exe /NOLICPROMPT /timer:0 C:\Users\WDAGUtilityAccount\Documents\tools\config.bgi
 
@@ -153,3 +169,18 @@ C:\Tools\sysinternals\Bginfo64.exe /NOLICPROMPT /timer:0 C:\Users\WDAGUtilityAcc
 if (Test-Path "C:\local\customize.ps1") {
     PowerShell.exe -ExecutionPolicy Bypass -File "C:\local\customize.ps1"
 }
+
+# Last commands
+& "C:\Program Files\7-Zip\7z.exe" x -aoa "C:\downloads\loki.zip" -o"C:\Program Files\"
+Add-ToUserPath "C:\Program Files\loki"
+& "C:\Program Files\7-Zip\7z.exe" x -pinfected "C:\downloads\signature.7z" -o"C:\Program Files\loki"
+mkdir "C:\data"
+Move-Item "C:\Program Files\loki\signature.yara" "C:\data"
+& "C:\Program Files\7-Zip\7z.exe" x -pinfected "C:\downloads\total.7z" -o"C:\data"
+
+# Start sysmon when installation is done
+if ( $env:WSDFIR_SYSMON -eq '"Yes"' ) {
+    & "$env:TOOLS\sysinternals\Sysmon64.exe" -accepteula -i "$env:WSDFIR_SYSMON_CONF"
+}
+
+Write-Output "helpers.ps1 done"
