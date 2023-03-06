5881e06d9989196e49a658f9980fe9c97e856c5d
Coding for fun <peter@reuteras.net>
2023-03-06 08:44:00 +0100

    Add example

--- local/example-customize.ps1
+++ local/example-customize.ps1
@@ -3,3 +3,6 @@
 
 # Customize background image:
 # PowerShell.exe -ExecutionPolicy Bypass -File C:\Users\WDAGUtilityAccount\Documents\tools\Update-Wallpaper.ps1 C:\local\image.png
+
+# Create extra shortcuts - third option is optional and is the start directory
+# Set-Shortcut "C:\Users\WDAGUtilityAccount\Desktop\debloat.lnk" "C:\Tools\bin\debloat.exe" "C:\Users\WDAGUtilityAccount\Desktop"
