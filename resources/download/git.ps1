2eebb51042255badfd1c64d39cac8f2287e2f77c
Coding for fun <peter@reuteras.net>
2023-03-09 13:53:44 +0100

    Add more repos

--- resources/download/git.ps1
+++ resources/download/git.ps1
@@ -12,11 +12,14 @@ Set-Location mount\git
 
 $repourls = `
     "https://github.com/keydet89/Events-Ripper.git", `
+    "https://github.com/last-byte/PersistenceSniper.git", `
     "https://github.com/Neo23x0/evt2sigma.git", `
     "https://github.com/pan-unit42/dotnetfile.git", `
     "https://github.com/reuteras/dfirws.wiki.git", `
     "https://github.com/SigmaHQ/sigma.git", `
-    "https://github.com/volexity/threat-intel.git"
+    "https://github.com/volexity/threat-intel.git", `
+    "https://github.com/wagga40/Zircolite.git", `
+    "https://github.com/Yamato-Security/hayabusa-rules.git"
 
 foreach ($repourl in $repourls) {
     $repo = Write-Output $repourl | ForEach-Object { $_ -replace "^.*/" } | ForEach-Object { $_ -replace "\.git$" }
