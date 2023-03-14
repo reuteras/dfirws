b99dfb3db47ea8addc02213a8d5b561693b242d4
Coding for fun <peter@reuteras.net>
2023-03-14 09:49:51 +0100

    Remove dynamic functions

--- setup/Microsoft.PowerShell_profile.ps1
+++ setup/Microsoft.PowerShell_profile.ps1
@@ -47,19 +47,3 @@ Set-Alias grep findstr
 Set-Alias tail Get-Content
 
 # Dynamicly added functions below
-function docx2txt() { python C:\venv\Scripts\docx2txt $PsBoundParameters.Values + $args }
-function prichunkpng() { python C:\venv\Scripts\prichunkpng $PsBoundParameters.Values + $args }
-function pricolpng() { python C:\venv\Scripts\pricolpng $PsBoundParameters.Values + $args }
-function priditherpng() { python C:\venv\Scripts\priditherpng $PsBoundParameters.Values + $args }
-function priforgepng() { python C:\venv\Scripts\priforgepng $PsBoundParameters.Values + $args }
-function prigreypng() { python C:\venv\Scripts\prigreypng $PsBoundParameters.Values + $args }
-function pripalpng() { python C:\venv\Scripts\pripalpng $PsBoundParameters.Values + $args }
-function pripamtopng() { python C:\venv\Scripts\pripamtopng $PsBoundParameters.Values + $args }
-function priplan9topng() { python C:\venv\Scripts\priplan9topng $PsBoundParameters.Values + $args }
-function pripnglsch() { python C:\venv\Scripts\pripnglsch $PsBoundParameters.Values + $args }
-function pripngtopam() { python C:\venv\Scripts\pripngtopam $PsBoundParameters.Values + $args }
-function prirowpng() { python C:\venv\Scripts\prirowpng $PsBoundParameters.Values + $args }
-function priweavepng() { python C:\venv\Scripts\priweavepng $PsBoundParameters.Values + $args }
-function spark-parser-coverage() { python C:\venv\Scripts\spark-parser-coverage $PsBoundParameters.Values + $args }
-function unpy2exe() { python C:\venv\Scripts\unpy2exe $PsBoundParameters.Values + $args }
-function vd() { python C:\venv\Scripts\vd $PsBoundParameters.Values + $args }
