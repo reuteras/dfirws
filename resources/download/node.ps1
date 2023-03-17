3d14460b261e1cfcb0524fc1bccbc658c7e9c44c
Coding for fun <peter@reuteras.net>
2023-03-17 05:53:20 +0100

    Use a sandbox to install Node npm packages

--- resources/download/node.ps1
+++ resources/download/node.ps1
@@ -1,6 +1,36 @@
-# Have to run after unpack
-Set-Location .\mount\Tools\node
-.\npm init -y >> ..\..\..\log\log.txt
-.\npm install --global deobfuscator >> ..\..\..\log\log.txt
-.\npm install --global jsdom >> ..\..\..\log\log.txt
-Set-Location ..\..\..
+param (
+    [String] $ScriptRoot=$PSScriptRoot
+)
+
+$ScriptRoot = "$ScriptRoot\resources\download"
+$ROOT_PATH=Resolve-Path "$ScriptRoot\..\..\"
+
+Write-Output "Setup node and install npm packages." > $ROOT_PATH\log\npm.txt
+
+. $ScriptRoot\common.ps1
+
+while(Get-Sandbox) {
+    Write-Output "Waiting for Sandbox to exit." > $ROOT_PATH\log\npm.txt
+    Start-Sleep 1 
+}
+
+if (! (Test-Path -Path "$ROOT_PATH\tmp" )) {
+    New-Item -ItemType Directory -Force -Path "$ROOT_PATH\tmp" > $null
+}
+
+if (! (Test-Path -Path "$ROOT_PATH\mount\Tools\node" )) {
+    New-Item -ItemType Directory -Force -Path "$ROOT_PATH\mount\Tools\node" > $null
+}
+
+if ( Test-Path -Path "$ROOT_PATH\tmp\node" ) {
+    Remove-Item -r -Force "$ROOT_PATH\tmp\node"
+}
+
+New-Item -ItemType Directory -Force -Path "$ROOT_PATH\tmp\node" > $null
+
+(Get-Content $ROOT_PATH\generate_node.wsb.template).replace('__SANDBOX__', "$ROOT_PATH") | Set-Content "$ROOT_PATH\generate_node.wsb"
+& "$ROOT_PATH\generate_node.wsb"
+Start-Sleep 10
+Remove-Item "$ROOT_PATH\generate_node.wsb"
+
+Stop-SandboxWhenDone "$ROOT_PATH\tmp\node\done"
