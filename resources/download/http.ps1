88b8a45b228faca847fccaa61585e6aa03a7b9cc
Coding for fun <peter@reuteras.net>
2023-03-17 05:08:47 +0100

    Fix URL

--- resources/download/http.ps1
+++ resources/download/http.ps1
@@ -20,7 +20,7 @@ Get-FileFromUri -uri "https://malcat.fr/latest/malcat_win64_lite.zip" -FilePath 
 Get-FileFromUri -uri "https://raw.githubusercontent.com/reuteras/yara-rules/master/signature/signature.7z" -FilePath ".\downloads\signature.7z"
 Get-FileFromUri -uri "https://raw.githubusercontent.com/reuteras/yara-rules/master/total/total.7z" -FilePath ".\downloads\total.7z"
 Get-FileFromUri -uri "https://raw.githubusercontent.com/hasherezade/pe2pic/master/pe2pic.py" -FilePath ".\downloads\pe2pic.py"
-Get-FileFromUri -uri "https://github.com/hasherezade/shellconv/blob/master/shellconv.py" -FilePath ".\downloads\shellconv.py"
+Get-FileFromUri -uri "https://raw.githubusercontent.com/hasherezade/shellconv/master/shellconv.py" -FilePath ".\downloads\shellconv.py"
 Get-FileFromUri -uri "https://www.nirsoft.net/utils/fulleventlogview-x64.zip" -FilePath ".\downloads\logview.zip"
 Get-FileFromUri -uri "https://downloads.pstwalker.com/pstwalker-portable" -FilePath ".\downloads\pstwalker.zip"
 Get-FileFromUri -uri "$choco" -FilePath ".\downloads\choco.zip"
