bb946edc8e80a1f845c4de22e12b194f345c6624
Coding for fun <peter@reuteras.net>
2023-03-23 11:07:13 +0100

    Fix log and make sure zstd is installed before continuing.

--- setup/install_bash.ps1
+++ setup/install_bash.ps1
@@ -13,23 +13,24 @@ mkdir "$TOOLS" > $null 2>&1
 
 Copy-Item "$SETUP_PATH\7zip.msi" "$TEMP\7zip.msi"
 Start-Process -Wait msiexec -ArgumentList "/i $TEMP\7zip.msi /qn /norestart"
-Get-Job | Receive-Job >> "C:\log\bash.txt" 2>&1
+Get-Job | Receive-Job | Out-Default >> "C:\log\bash.txt" 2>&1
 
 Write-Output "Get-Content C:\log\bash.txt -Wait" | Out-File -FilePath "C:\Progress.ps1" -Encoding "ascii"
 Write-Output "PowerShell.exe -ExecutionPolicy Bypass -File C:\Progress.ps1" | Out-File -FilePath "$HOME\Desktop\Progress.cmd" -Encoding "ascii"
 
 # Update path
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
-& "$env:ProgramFiles\7-Zip\7z.exe" x -aoa "$SETUP_PATH\zstd.zip" -o"$TOOLS\zstd" >> "C:\log\bash.txt" 2>&1
+Start-Process -Wait "$env:ProgramFiles\7-Zip\7z.exe" -ArgumentList "x -aoa $SETUP_PATH\zstd.zip -o$TOOLS\zstd" 
+Get-Job | Receive-Job | Out-Default >> "C:\log\bash.txt" 2>&1
 
-Set-Location C:\bash >> "C:\log\bash.txt" 2>&1
-Remove-Item -Force *.tar >> "C:\log\bash.txt" 2>&1
+Set-Location C:\bash
+Remove-Item -Force *.tar
 Get-ChildItem -Include * |
     ForEach-Object {
         Start-Process -Wait "$TOOLS\zstd\zstd.exe" -ArgumentList "-d $_"
     }
 
-Get-Job | Receive-Job >> "C:\log\bash.txt" 2>&1
+Get-Job | Receive-Job | Out-Default >> "C:\log\bash.txt" 2>&1
 Write-Output "Uncompress of zst files done." >> "C:\log\bash.txt" 2>&1
 Write-Output "" > C:\bash\done
 
