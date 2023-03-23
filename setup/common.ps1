99f52a97575a1c831607410ee38cb2638852203c
Coding for fun <peter@reuteras.net>
2023-03-23 06:50:05 +0100

    Move helper functions to separate file.
    Makes it possible to reuse them in local scripts.

---
+++ setup/common.ps1
@@ -0,0 +1,44 @@
+# Declare helper functions
+function Add-ToUserPath {
+    param (
+        [Parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $dir
+    )
+
+    #$dir = (Resolve-Path $dir)
+
+    $path = [Environment]::GetEnvironmentVariable("PATH", [System.EnvironmentVariableTarget]::User)
+    if (!($path.Contains($dir))) {
+        # append dir to path
+        [Environment]::SetEnvironmentVariable("PATH", $path + ";$dir", [EnvironmentVariableTarget]::User)
+        Write-Output "Added $dir to PATH"
+        return
+    }
+    Write-Error "$dir is already in PATH"
+}
+
+function Add-Shortcut {
+    param (
+        [string]$SourceLnk,
+        [string]$DestinationPath,
+        [string]$WorkingDirectory
+    )
+    $WshShell = New-Object -comObject WScript.Shell
+    $Shortcut = $WshShell.CreateShortcut($SourceLnk)
+    if ($WorkingDirectory -ne $Null) {
+        $Shortcut.WorkingDirectory = $WorkingDirectory
+    }
+    $Shortcut.TargetPath = $DestinationPath
+    $Shortcut.Save()
+}
+
+function Update-WallPaper {
+    [CmdletBinding()]
+    param (
+        [parameter(Mandatory = $true)][String]$path
+    )
+    C:\Users\WDAGUtilityAccount\Documents\tools\Update-WallPaper $path
+    C:\Tools\sysinternals\Bginfo64.exe /NOLICPROMPT /timer:0 $HOME\Documents\tools\config.bgi
+}
