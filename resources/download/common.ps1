2a61ac5262ffdf9ac5fc23420ae3675ccd7ba16b
Coding for fun <peter@reuteras.net>
2023-03-23 13:13:01 +0100

    Rewrite with ChatGPT

--- resources/download/common.ps1
+++ resources/download/common.ps1
@@ -1,27 +1,34 @@
+# Set the default encoding for Out-File to UTF-8
 $PSDefaultParameterValues['Out-File:Encoding'] = 'utf8'
 
+# Function to download a file from a given URI and save it to a specified file path
 function Get-FileFromUri {
     Param (
         [Parameter(Mandatory=$True)] [string]$uri,
         [Parameter(Mandatory=$True)] [string]$FilePath
     )
 
+    # Remove any leading '..' from the file path and set up temporary file path and final file path
     $CleanPath = $FilePath -replace "^..", ""
     $TmpFilePath= "$PSScriptRoot\..\..\tmp\$CleanPath"
     $FilePath = "$PSScriptRoot\..\..\$CleanPath"
 
+    # Ensure the directory for the final file path exists
     If (! ( Test-Path ([System.IO.FileInfo]$FilePath).DirectoryName ) ) {
         New-Item ([System.IO.FileInfo]$FilePath).DirectoryName -force -type directory | Out-Null
     }
 
+    # Ensure the directory for the temporary file path exists
     If (! ( Test-Path ([System.IO.FileInfo]$TmpFilePath).DirectoryName ) ) {
         New-Item ([System.IO.FileInfo]$TmpFilePath).DirectoryName -force -type directory | Out-Null
     }
 
+    # Set the number of retries for the download
     $retries = 3
     $ProgressPreference = 'SilentlyContinue'
     while($true) {
         try {
+            # Attempt to download the file from the specified URI
             Invoke-WebRequest $uri -UserAgent "Wget x64" -OutFile $TmpFilePath
             Write-SynchronizedLog "Downloaded $uri to $FilePath."
             break
@@ -33,25 +40,27 @@ function Get-FileFromUri {
                 $retries--
                 Write-SynchronizedLog "Waiting 10 seconds before retrying. Retries left: $retries"
                 Start-Sleep -Seconds 10
-
             } else {
                 $exception = $_.Exception
                 throw $exception
             }
         }
     }
+    # Copy the temporary file to the final file path and remove the temporary file
     $result = rclone copyto --verbose --checksum $TmpFilePath $FilePath 2>&1 | Out-String
     Write-SynchronizedLog "$result"
     Remove-Item $TmpFilePath
     $ProgressPreference = 'Continue'
 }
 
+# Function to get the download URL for a GitHub release
 function Get-DownloadUrl {
     Param (
         [string]$releases,
         [string]$match
     )
     try {
+        # Retrieve the download URLs and filter them based on the provided regex match
         $downloads = (Invoke-WebRequest $releases | ConvertFrom-Json)[0].assets.browser_download_url
         if ( ( Write-Output $downloads | Measure-Object -word ).Words -gt 1 ) {
             return Write-Output $downloads | findstr /R $match | findstr /R /V "darwin sig blockmap"
@@ -64,6 +73,7 @@ function Get-DownloadUrl {
     }
 }
 
+# Function to download the specified GitHub release and save it to a specified file path
 function Get-GitHubRelease {
     Param (
         [string]$repo,
@@ -71,17 +81,19 @@ function Get-GitHubRelease {
         [string]$match
     )
 
+    # Construct the URL to get the latest release information
     $releases = "https://api.github.com/repos/$repo/releases/latest"
 
+    # Get the download URL by matching the specified regex pattern
     $url = Get-DownloadUrl -Releases $releases -Match $match
 
-
+    # If no download URL is found, try without "latest"
     if ( !$url ) {
-        # Try without latest
         $releases = "https://api.github.com/repos/$repo/releases"
         $url = Get-DownloadUrl -Releases $releases -Match $match
     }
 
+    # If still no download URL is found, try getting the tarball URL
     if ( !$url ) {
         $url = curl --silent https://api.github.com/repos/$repo/releases | findstr tarball | Select-Object -First 1 | ForEach-Object { ($_ -split "\s+")[2] } | ForEach-Object { ($_ -replace '[",]','') }
         if ( !$url) {
@@ -90,15 +102,18 @@ function Get-GitHubRelease {
         }
     }
 
+    # Log the chosen URL and download the file
     Write-SynchronizedLog "Using $url for $repo."
     Get-FileFromUri -uri $url -FilePath $path
 }
 
+# Function to get the download URL for a Chocolatey package
 function Get-ChocolateyUrl {
     Param (
         [string]$package
     )
 
+    # Scrape the download URL from the Chocolatey package page
     $url = curl --silent https://community.chocolatey.org/packages/$package | findstr /C:"Download the raw" | findstr ">Download<" | ForEach-Object { ($_ -split '"' )[1] }
 
     if (!$url) {
@@ -109,6 +124,7 @@ function Get-ChocolateyUrl {
     return $url
 }
 
+# Function to get the download URL from a specified web page, based on a regex pattern
 function Get-DownloadUrlFromPage {
     [CmdletBinding()]
     param (
@@ -123,6 +139,7 @@ function Get-DownloadUrlFromPage {
     return Invoke-WebRequest -Uri "$url" -UseBasicParsing | Select-String -Pattern "$regex" | Select-Object -ExpandProperty Matches | Select-Object -ExpandProperty Value
 }
 
+# Function to get the download URL for an MSYS package
 function Get-DownloadUrlMSYS {
     [CmdletBinding()]
     param (
@@ -157,6 +174,7 @@ function Get-DownloadUrlMSYS {
     return $base + $url.FileName
 }
 
+# Function to stop the Windows Sandbox when the specified file is deleted
 function Stop-SandboxWhenDone {
     [CmdletBinding(SupportsShouldProcess)]
     param (
@@ -165,7 +183,7 @@ function Stop-SandboxWhenDone {
         [System.Threading.Mutex] $Mutex
     )
 
-    while ($true ) {
+    while ($true) {
         $status = Get-Process WindowsSandboxClient 2> $null
         if ($status) {
             if ( Test-Path $path ) {
@@ -185,9 +203,9 @@ function Stop-SandboxWhenDone {
             return
         }
     }
-
 }
 
+# Function to write a synchronized log to a specified file
 function Write-SynchronizedLog {
     param (
         [string]$Message,
