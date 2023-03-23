233383ff60b47abd499d42764ee7795b5027a8e1
Coding for fun <peter@reuteras.net>
2023-03-23 06:50:48 +0100

    Update example code

--- local/example-customize.ps1
+++ local/example-customize.ps1
@@ -1,11 +1,17 @@
 # Rename to customize.ps1 to use.
 # Add files to this folder and they will be available as C:\local
 
+# Source common functions
+. C:\Users\WDAGUtilityAccount\Documents\tools\common.ps1
+
 # Customize background image:
-# PowerShell.exe -ExecutionPolicy Bypass -File C:\Users\WDAGUtilityAccount\Documents\tools\Update-Wallpaper.ps1 C:\local\image.png
+# Update-Wallpaper "C:\local\image.jpg"
 
 # Create extra shortcuts - third option is optional and is the start directory
 # Set-Shortcut "C:\Users\WDAGUtilityAccount\Desktop\debloat.lnk" "C:\Tools\bin\debloat.exe" "C:\Users\WDAGUtilityAccount\Desktop"
+
+# Add new path for user
+# Add-ToUserPath "C:\local\bin"
 
 # Chocolately
 # You must have 'set SET WSDFIR_CHOCO="Yes"' in your setup\config.txt
