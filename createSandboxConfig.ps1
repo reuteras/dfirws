dae1b4e33cb8cea15f45f26d96201267287e2eb0
Peter Reuterås <peter@reuteras.com>
2023-03-26 13:40:46 +0200

    Add config for sandbox with networking.

--- createSandboxConfig.ps1
+++ createSandboxConfig.ps1
@@ -1,5 +1,6 @@
 # Create default files and configuration
 (Get-Content dfirws.wsb.template).replace('__SANDBOX__', $PSScriptRoot) | Set-Content .\dfirws.wsb
+(Get-Content network_dfirws.wsb.template).replace('__SANDBOX__', $PSScriptRoot) | Set-Content .\network_dfirws.wsb
 
 if (! (Test-Path -Path .\setup\config.txt)) {
     Copy-Item .\setup\default-config.txt .\setup\config.txt
