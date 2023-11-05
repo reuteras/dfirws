d4edc675206c3e8a00a61207a01cd6a334974f72
Coding for fun <peter@reuteras.net>
2023-11-05 12:44:25 +0100

    Add lssmsi and reorder. REmove UniExtract2

--- resources/download/release.ps1
+++ resources/download/release.ps1
@@ -3,7 +3,7 @@
 Write-DateLog "Download releases from GitHub."
 
 Get-GitHubRelease -repo "3lp4tr0n/BeaconHunter" -path ".\downloads\beaconhunter.zip" -match BeaconHunter.zip
-Get-GitHubRelease -repo "Bioruebe/UniExtract2" -path ".\downloads\uniextract2.zip" -match UniExtract
+Get-GitHubRelease -repo "activescott/lessmsi" -path ".\downloads\lessmsi.zip" -match lessmsi-v
 Get-GitHubRelease -repo "brimdata/zui" -path ".\downloads\zui.exe" -match Zui-Setup
 Get-GitHubRelease -repo "BurntSushi/ripgrep" -path ".\downloads\ripgrep.zip" -match x86_64-pc-windows-msvc
 Get-GitHubRelease -repo "c3rb3ru5d3d53c/binlex" -path ".\downloads\binlex.zip" -match windows
@@ -11,11 +11,15 @@ Get-GitHubRelease -repo "cmderdev/cmder" -path ".\downloads\cmder.7z" -match cmd
 Get-GitHubRelease -repo "dnSpyEx/dnSpy" -path ".\downloads\dnSpy32.zip" -match win32
 Get-GitHubRelease -repo "dnSpyEx/dnSpy" -path ".\downloads\dnSpy64.zip" -match win64
 Get-GitHubRelease -repo "facebook/zstd" -path ".\downloads\zstd.zip" -match win64
+Get-GitHubRelease -repo "gchq/CyberChef" -path ".\downloads\CyberChef.zip" -match CyberChef
+Get-GitHubRelease -repo "git-for-windows/git" -path ".\downloads\git.exe" -match 64-bit.exe
+Get-GitHubRelease -repo "gollum/gollum" -path ".\downloads\gollum.war" -match gollum.war
 Get-GitHubRelease -repo "harelsegev/INDXRipper" -path ".\downloads\indxripper.zip" -match amd64.zip
 Get-GitHubRelease -repo "hasherezade/pe-bear" -path ".\downloads\pebear.zip" -match x64_win_vs17.zip
 Get-GitHubRelease -repo "horsicq/DIE-engine" -path ".\downloads\die.zip" -match die_win64_portable
-Get-GitHubRelease -repo "gchq/CyberChef" -path ".\downloads\CyberChef.zip" -match CyberChef
-Get-GitHubRelease -repo "git-for-windows/git" -path ".\downloads\git.exe" -match 64-bit.exe
+Get-GitHubRelease -repo "horsicq/XELFViewer" -path ".\downloads\XELFViewer.zip" -match win64_portable
+Get-GitHubRelease -repo "java-decompiler/jd-gui" -path ".\downloads\jd-gui.zip" -match jd-gui-windows
+Get-GitHubRelease -repo "jqlang/jq" -path ".\downloads\jq.exe" -match win64
 Get-GitHubRelease -repo "kacos2000/Jumplist-Browser" -path ".\downloads\JumplistBrowser.exe" -match JumplistBrowser.exe
 Get-GitHubRelease -repo "kacos2000/Prefetch-Browser" -path ".\downloads\PrefetchBrowser.exe" -match PrefetchBrowser.exe
 Get-GitHubRelease -repo "Konloch/bytecode-viewer" -path ".\downloads\BCV.jar" -match Bytecode
@@ -24,6 +28,7 @@ Get-GitHubRelease -repo "mandiant/capa" -path ".\downloads\capa-windows.zip" -ma
 Get-GitHubRelease -repo "mandiant/flare-floss" -path ".\downloads\floss.zip" -match windows
 Get-GitHubRelease -repo "mandiant/flare-fakenet-ng" -path ".\downloads\fakenet.zip" -match fakenet
 Get-GitHubRelease -repo "mandiant/GoReSym" -path ".\downloads\GoReSym.zip" -match GoReSym-windows
+Get-GitHubRelease -repo "mentebinaria/elfparser-ng" -path ".\downloads\elfparser-ng.zip" -match win64
 Get-GitHubRelease -repo "multiprocessio/dsq" -path ".\downloads\dsq.zip" -match dsq-win
 Get-GitHubRelease -repo "NationalSecurityAgency/ghidra" -path ".\downloads\ghidra.zip" -match ghidra
 Get-GitHubRelease -repo "nccgroup/MetadataPlus" -path ".\downloads\MetadataPlus.exe" -match MetadataPlus
@@ -36,12 +41,13 @@ Get-GitHubRelease -repo "PowerShell/vscode-powershell" -path ".\downloads\vscode
 Get-GitHubRelease -repo "qpdf/qpdf" -path ".\downloads\qpdf.zip" -match msvc64.zip
 Get-GitHubRelease -repo "radareorg/radare2" -path ".\downloads\radare2.zip" -match w64.zip
 Get-GitHubRelease -repo "rizinorg/cutter" -path ".\downloads\cutter.zip" -match Windows-x86_64.zip
-Get-GitHubRelease -repo "Squiblydoo/debloat" -path ".\downloads\debloat.zip" -match Win64
-Get-GitHubRelease -repo "jqlang/jq" -path ".\downloads\jq.exe" -match win64
+Get-GitHubRelease -repo "skylot/jadx" -path ".\downloads\jadx.zip" -match jadx-1
+Get-GitHubRelease -repo "Squiblydoo/debloat" -path ".\downloads\debloat.zip" -match Windows
 Get-GitHubRelease -repo "streetsidesoftware/vscode-spell-checker" -path ".\downloads\vscode\vscode-spell-checker.vsix" -match vsix
 Get-GitHubRelease -repo "thumbcacheviewer/thumbcacheviewer" -path ".\downloads\thumbcacheviewer.zip" -match viewer_64
 Get-GitHubRelease -repo "upx/upx" -path ".\downloads\upx.zip" -match win64
 Get-GitHubRelease -repo "wader/fq" -path ".\downloads\fq.zip" -match windows_amd64.zip
+Get-GitHubRelease -repo "WerWolv/ImHex" -path ".\downloads\imhex.zip" -match Windows-x86_64.msi
 Get-GitHubRelease -repo "WithSecureLabs/chainsaw" -path ".\downloads\chainsaw.zip" -match x86_64-pc-windows-msvc
 Get-GitHubRelease -repo "VirusTotal/yara" -path ".\downloads\yara.zip" -match win64
 Get-GitHubRelease -repo "Yamato-Security/hayabusa" -path ".\downloads\hayabusa.zip" -match win-64
