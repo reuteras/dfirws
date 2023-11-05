6eced2533e9c262ac15f91382b994b56e2d42a2c
Coding for fun <peter@reuteras.net>
2023-11-05 12:39:15 +0100

    Add scripts to use Gollum

---
+++ setup/utils/gollum.bat
@@ -0,0 +1,1 @@
+powershell -executionpolicy bypass -file C:\Users\WDAGUtilityAccount\Documents\tools\utils\gollum.ps1
---
+++ setup/utils/gollum.ps1
@@ -0,0 +1,1 @@
+& "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" --profile-directory=Default http://localhost:4567/Home
