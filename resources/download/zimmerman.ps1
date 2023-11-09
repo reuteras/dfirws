1b43926d9bf178ba3bdd8a0b3b87aa9b4750c2ed
Coding for fun <peter@reuteras.net>
2023-11-09 10:46:38 +0100

    Copy in this file

--- resources/download/zimmerman.ps1
+++ resources/download/zimmerman.ps1
@@ -1,5 +1,10 @@
 . $PSScriptRoot\common.ps1
 
+$TOOLS=".\mount\Tools"
+$SETUP_PATH=".\downloads"
+
 Write-DateLog "Download Zimmerman tools."
 
 .\resources\download\Get-ZimmermanTools.ps1 -Dest .\downloads\Zimmerman >> .\log\log.txt 2>&1
+
+xcopy /E $SETUP_PATH\Zimmerman $TOOLS\Zimmerman | Out-Null
