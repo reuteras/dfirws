8191fa2b1e8abd2ca137653e1dc5c1056a8bd1a0
Coding for fun <peter@reuteras.net>
2023-11-12 14:21:48 +0100

    Update included git repos

--- resources/download/git.ps1
+++ resources/download/git.ps1
@@ -11,23 +11,32 @@ New-Item -ItemType Directory -Force -Path mount\git > $null
 Set-Location mount\git
 
 $repourls = `
+    "https://github.com/Bw3ll/sharem.git", `
+    "https://github.com/crypto2011/IDR.git", `
     "https://github.com/cyberark/White-Phoenix.git", `
     "https://github.com/gehaxelt/Python-dsstore.git", `
+    "https://github.com/JavierYuste/radare2-deep-graph.git", `
     "https://github.com/joeavanzato/Trawler.git", `
     "https://github.com/keydet89/Events-Ripper.git", `
     "https://github.com/keydet89/RegRipper3.0", `
     "https://github.com/last-byte/PersistenceSniper.git", `
+    "https://github.com/MacDue/ssdeep-windows-32_64.git", `
     "https://github.com/Malandrone/PowerDecode.git", `
+    "https://github.com/mari-mari/CapaExplorer.git", `
     "https://github.com/MarkBaggett/ese-analyst.git", `
     "https://github.com/Neo23x0/evt2sigma.git", `
     "https://github.com/netspooky/scare.git", `
+    "https://github.com/ninewayhandshake/capa-explorer.git", `
     "https://github.com/pan-unit42/dotnetfile.git", `
     "https://github.com/reuteras/dfirws.wiki.git", `
+    "https://github.com/rizinorg/cutter-jupyter.git", `
     "https://github.com/SigmaHQ/sigma.git", `
     "https://github.com/volexity/one-extract.git", `
     "https://github.com/volexity/threat-intel.git", `
     "https://github.com/wagga40/Zircolite.git", `
-    "https://github.com/Yamato-Security/hayabusa-rules.git"
+    "https://github.com/xaitax/PatchaPalooza.git", `
+    "https://github.com/Yamato-Security/hayabusa-rules.git", `
+    "https://github.com/yossizap/cutterref.git"
 
 foreach ($repourl in $repourls) {
     $repo = Write-Output $repourl | ForEach-Object { $_ -replace "^.*/" } | ForEach-Object { $_ -replace "\.git$" }
@@ -43,3 +52,6 @@ foreach ($repourl in $repourls) {
 }
 
 Set-Location ..\..
+
+# Copy files
+Copy-Item .\mount\git\CapaExplorer\capaexplorer.py ./mount/Tools/ghidra/Ghidra/Features/Python/ghidra_scripts
