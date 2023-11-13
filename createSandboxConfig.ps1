3b45c627d683a5e4ecfd3119b26d37fd503dc29c
Coding for fun <peter@reuteras.net>
2023-11-13 07:11:09 +0100

    Create customize.ps1 and check for existents of wsb files

--- createSandboxConfig.ps1
+++ createSandboxConfig.ps1
@@ -1,8 +1,19 @@
 # Create default files and configuration
-(Get-Content dfirws.wsb.template).replace('__SANDBOX__', $PSScriptRoot) | Set-Content .\dfirws.wsb
-(Get-Content network_dfirws.wsb.template).replace('__SANDBOX__', $PSScriptRoot) | Set-Content .\network_dfirws.wsb
 
-if (! (Test-Path -Path .\setup\config.txt)) {
-    Copy-Item .\setup\default-config.txt .\setup\config.txt
+if (! (Test-Path -Path ".\dfirws.wsb")) {
+    (Get-Content dfirws.wsb.template).replace('__SANDBOX__', $PSScriptRoot) | Set-Content .\dfirws.wsb
+}
+
+if (! (Test-Path -Path ".\network_dfirws.wsb")) {
+    (Get-Content network_dfirws.wsb.template).replace('__SANDBOX__', $PSScriptRoot) | Set-Content .\network_dfirws.wsb
+}
+
+if (! (Test-Path -Path ".\setup\config.txt")) {
+    Copy-Item ".\setup\default-config.txt" ".\setup\config.txt"
     Write-Output "Created .\tools\config.txt. Select tools to install based on your needs."
+}
+
+if (! (Test-Path -Path ".\local\customize.ps1")) {
+    Copy-Item ".\local\example-customize.txt" ".\local\customize.ps1"
+    Write-Output "Created .\local\customize.ps1. Customize shortcuts on desktop, wallpaper and more."
 }
