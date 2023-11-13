9566f9f268c46d375606edf3346b3e1aab3b245a
Coding for fun <peter@reuteras.net>
2023-11-13 07:10:44 +0100

    Create shortcuts here

--- local/example-customize.ps1
+++ local/example-customize.ps1
@@ -2,13 +2,26 @@
 # Add files to this folder and they will be available as C:\local
 
 # Source common functions
-. C:\Users\WDAGUtilityAccount\Documents\tools\common.ps1
+. $HOME\Documents\tools\wscommon.ps1
 
 # Customize background image:
 # Update-Wallpaper "C:\local\image.jpg"
 
 # Create extra shortcuts - third option is optional and is the start directory
-# Set-Shortcut "C:\Users\WDAGUtilityAccount\Desktop\debloat.lnk" "C:\Tools\bin\debloat.exe" "C:\Users\WDAGUtilityAccount\Desktop"
+# Set-Shortcut "$HOME\Desktop\debloat.lnk" "C:\Tools\bin\debloat.exe" "$HOME\Desktop"
+
+#Add-Shortcut -SourceLnk "$HOME\Desktop\bash.lnk" -DestinationPath "$env:ProgramFiles\Git\bin\bash.exe" -WorkingDirectory "$HOME\Desktop"
+Add-Shortcut -SourceLnk "$HOME\Desktop\CyberChef.lnk" -DestinationPath "C:\Tools\CyberChef\CyberChef.html"
+#Add-Shortcut -SourceLnk "$HOME\Desktop\cmder.lnk" -DestinationPath "$env:ProgramFiles\cmder\cmder.exe" -WorkingDirectory "$HOME\Desktop"
+#Add-Shortcut -SourceLnk "$HOME\Desktop\Cutter.lnk" -DestinationPath "C:\Tools\cutter\cutter.exe"
+#Add-Shortcut -SourceLnk "$HOME\Desktop\FullEventLogView.lnk" -DestinationPath "C:\Tools\FullEventLogView\FullEventLogView.exe"
+#Add-Shortcut -SourceLnk "$HOME\Desktop\ghidraRun.lnk" -DestinationPath "C:\Tools\ghidra\ghidraRun.bat"
+#Add-Shortcut -SourceLnk "$HOME\Desktop\HxD.lnk" -DestinationPath "$env:ProgramFiles\HxD\HxD.exe"
+#Add-Shortcut -SourceLnk "$HOME\Desktop\msgviewer.lnk" -DestinationPath "C:\Tools\lib\msgviewer.jar"
+#Add-Shortcut -SourceLnk "$HOME\Desktop\Malcat.lnk" -DestinationPath "C:\Tools\Malcat\bin\malcat.exe"
+Add-Shortcut -SourceLnk "$HOME\Desktop\Notepad++.lnk" -DestinationPath "$env:ProgramFiles\Notepad++\notepad++.exe"
+#Add-Shortcut -SourceLnk "$HOME\Desktop\PE-bear.lnk" -DestinationPath "C:\Tools\pebear\PE-bear.exe"
+#Add-Shortcut -SourceLnk "$HOME\Desktop\pestudio.lnk" -DestinationPath "C:\Tools\pestudio\pestudio\pestudio.exe"
 
 # Add new path for user
 # Add-ToUserPath "C:\local\bin"
