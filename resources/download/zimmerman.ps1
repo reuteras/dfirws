eb027a8e1fa451f2ffb1d0e37d0eb51b2082c4de
Coding for fun <peter@reuteras.net>
2023-11-17 12:45:08 +0100

    Move output to downloadFiles

--- resources/download/zimmerman.ps1
+++ resources/download/zimmerman.ps1
@@ -1,7 +1,5 @@
 . $PSScriptRoot\common.ps1
 
-Write-DateLog "Download Zimmerman tools."
-
 .\resources\download\Get-ZimmermanTools.ps1 -Dest .\downloads\Zimmerman >> .\log\log.txt 2>&1
 
 xcopy /E $SETUP_PATH\Zimmerman $TOOLS\Zimmerman | Out-Null
