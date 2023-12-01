5984f33692ffb6c949bdab04b29c81faac22e373
Coding for fun <peter@reuteras.net>
2023-12-01 13:18:39 +0100

    Add notebook for PE and common.py.

---
+++ setup/jupyter/common.py
@@ -0,0 +1,17 @@
+#
+# Common functions for notebooks
+#
+
+import os
+import subprocess
+from pathlib import Path
+
+READONLY = Path(os.getenv("USERPROFILE") + "/Desktop/readonly")
+
+
+def execute7z(zipPathInput, zipPassword, zipPathOutput):
+    """Excecute 7zip batch command using subprocess call"""
+    process = subprocess.Popen([r"7z.exe", "x", zipPathInput, "-p{}".format(zipPassword), "-o{}".format(zipPathOutput)])
+    process.wait()
+    process.kill()
+    return 0
