dc415d266d4b13aa6e2f95c2a6d4d1ccf0626cb8
Coding for fun <reuteras@users.noreply.github.com>
2023-12-01 14:07:56 +0100

    linting common.py

--- setup/jupyter/common.py
+++ setup/jupyter/common.py
@@ -11,8 +11,15 @@ READONLY = Path(os.getenv("USERPROFILE") + "/Desktop/readonly")
 
 def execute7z(zipPathInput, zipPassword, zipPathOutput):
     """Excecute 7zip batch command using subprocess call"""
-    process = subprocess.Popen([r"7z.exe", "x", zipPathInput, "-p{}".format(zipPassword), "-o{}".format(zipPathOutput)])
+    process = subprocess.Popen(
+        [
+            r"7z.exe",
+            "x",
+            zipPathInput,
+            "-p{}".format(zipPassword),
+            "-o{}".format(zipPathOutput)
+        ]
+    )
     process.wait()
     process.kill()
     return 0
- 
