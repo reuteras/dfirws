ed03a702f8d997083b0161619925b804dc273f18
Coding for fun <peter@reuteras.net>
2023-12-08 13:25:57 +0100

    Add more enrichment sources

--- enrichment.ps1
+++ enrichment.ps1
@@ -3,6 +3,9 @@
 # - Tor exit nodes
 # - Maxmind GeoLite2 ASN database
 
+# Set the currentDirectory to the current directory
+$currentDirectory = "${PWD}"
+
 $enrichmentDirectory = "${PWD}\downloads\enrichment"
 
 # Create the enrichment directory if it doesn't exist
@@ -27,10 +30,23 @@ foreach ($file in $files) {
     $fileUrl = $folderUrl + $file
     $savePath = Join-Path -Path "${PWD}${torsaveDirectory}" -ChildPath "${file}"
     Write-Output "Downloading $fileUrl to $savePath"
-    #$webClient.DownloadFile("${fileUrl}", "${savePath}")
+    $webClient.DownloadFile("${fileUrl}", "${savePath}")
+}
+$webClient.Dispose()
+
+$DATE = Get-Date -Format "yyyy-MM-dd"
+
+# Get manuf file for MAC address lookup
+$manufSaveDirectory = "${enrichmentDirectory}\manuf"
+if (-not (Test-Path -Path $manufSaveDirectory)) {
+    New-Item -ItemType Directory -Path $manufSaveDirectory -Force | Out-Null
 }
 
-$webClient.Dispose()
+$manufUrl = "https://www.wireshark.org/download/automated/data/manuf"
+$manufSavePath = Join-Path -Path "${manufSaveDirectory}" -ChildPath "manuf.txt"
+Write-Output "Downloading $manufUrl to $manufSavePath"
+Invoke-WebRequest -Uri $manufUrl -OutFile $manufSavePath
+Copy-Item -Path $manufSavePath -Destination "${manufSaveDirectory}\manuf-${DATE}.txt"
 
 if (-not (Test-Path -Path "${PWD}\config.ps1")) {
     Write-Output "Please create a config.ps1 to download Maxmind databases"
@@ -42,21 +58,96 @@ if (-not (Test-Path -Path "${PWD}\config.ps1")) {
 if (-not $MAXMIND_LICENSE_KEY) {
     Write-Output "Please set the MAXMIND_LICENSE_KEY variable in config.ps1 if you want to download Maxmind databases"
 } else {
+    $maxmindSaveDirectory = "${enrichmentDirectory}\maxmind"
+
+    # Create the save directory for maxmind if it doesn't exist
+    if (-not (Test-Path -Path $maxmindSaveDirectory)) {
+        New-Item -ItemType Directory -Path $maxmindSaveDirectory -Force | Out-Null
+    }
+
     # Download Maxmind GeoLite2 ASN database
     $folderUrl = "https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-ASN&license_key=${MAXMIND_LICENSE_KEY}&suffix=tar.gz"
-    $savePath = Join-Path -Path "${enrichmentDirectory}" -ChildPath "GeoLite2-ASN.tar.gz"
+    $savePath = Join-Path -Path "${maxmindSaveDirectory}" -ChildPath "GeoLite2-ASN.tar.gz"
     Write-Output "Downloading GeoLite2-ASN.tar.gz"
     Invoke-WebRequest -Uri $folderUrl -OutFile $savePath
+    Copy-Item -Path $savePath -Destination "${maxmindSaveDirectory}\GeoLite2-ASN-${DATE}.tar.gz"
 
     # Download Maxmind GeoLite2 City database
     $folderUrl = "https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-City&license_key=${MAXMIND_LICENSE_KEY}&suffix=tar.gz"
-    $savePath = Join-Path -Path "${enrichmentDirectory}" -ChildPath "GeoLite2-City.tar.gz"
+    $savePath = Join-Path -Path "${maxmindSaveDirectory}" -ChildPath "GeoLite2-City.tar.gz"
     Write-Output "Downloading GeoLite2-City.tar.gz"
     Invoke-WebRequest -Uri $folderUrl -OutFile $savePath
+    Copy-Item -Path $savePath -Destination "${maxmindSaveDirectory}\GeoLite2-City-${DATE}.tar.gz"
 
     # Download Maxmind GeoLite2 Country database
     $folderUrl = "https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-Country&license_key=${MAXMIND_LICENSE_KEY}&suffix=tar.gz"
-    $savePath = Join-Path -Path "${enrichmentDirectory}" -ChildPath "GeoLite2-Country.tar.gz"
+    $savePath = Join-Path -Path "${maxmindSaveDirectory}" -ChildPath "GeoLite2-Country.tar.gz"
     Write-Output "Downloading GeoLite2-Country.tar.gz"
     Invoke-WebRequest -Uri $folderUrl -OutFile $savePath
+    Copy-Item -Path $savePath -Destination "${maxmindSaveDirectory}\GeoLite2-Country-${DATE}.tar.gz"
 }
+
+# Download the latest version of Suricata rules
+$suricataUrl = "https://rules.emergingthreats.net/open/suricata-5.0/emerging.rules.zip"
+$suricataSaveDirectory = "${enrichmentDirectory}\suricata"
+
+# Create the save directory for suricata if it doesn't exist
+if (-not (Test-Path -Path $suricataSaveDirectory)) {
+    New-Item -ItemType Directory -Path $suricataSaveDirectory -Force | Out-Null
+}
+
+$suricataSavePath = Join-Path -Path "${suricataSaveDirectory}" -ChildPath "emerging.rules.zip"
+Write-Output "Downloading $suricataUrl to $suricataSavePath"
+Invoke-WebRequest -Uri $suricataUrl -OutFile $suricataSavePath
+Copy-Item -Path $suricataSavePath -Destination "${suricataSaveDirectory}\emerging-${DATE}.rules.zip"
+
+# Download the latest version of Snort rules
+$snortUrl = "https://www.snort.org/downloads/community/community-rules.tar.gz"
+$snortSaveDirectory = "${enrichmentDirectory}\snort"
+
+# Create the save directory for snort if it doesn't exist
+if (-not (Test-Path -Path $snortSaveDirectory)) {
+    New-Item -ItemType Directory -Path $snortSaveDirectory -Force | Out-Null
+}
+
+$snortSavePath = Join-Path -Path "${snortSaveDirectory}" -ChildPath "community-rules.tar.gz"
+Write-Output "Downloading $snortUrl to $snortSavePath"
+Invoke-WebRequest -Uri $snortUrl -OutFile $snortSavePath
+Copy-Item -Path $snortSavePath -Destination "${snortSaveDirectory}\community-rules-${DATE}.rules.tar.gz"
+
+# Git repositories for enrichment
+if (! (Get-Command git )) {
+    Write-Output "Need git to checkout git repositories."
+    Exit
+}
+
+$gitSaveDirectory = "${enrichmentDirectory}\git"
+if (-not (Test-Path -Path $gitSaveDirectory)) {
+    New-Item -ItemType Directory -Path $gitSaveDirectory -Force | Out-Null
+}
+Set-Location $gitSaveDirectory
+
+$repourls = `
+    "https://github.com/X4BNet/lists_vpn.git", `
+    "https://github.com/X4BNet/lists_torexit.git", `
+    "https://github.com/X4BNet/lists_stopforumspam.git", `
+    "https://github.com/X4BNet/lists_searchengine.git", `
+    "https://github.com/X4BNet/lists_bots.git", `
+    "https://github.com/X4BNet/lists_uptimerobot.git", `
+    "https://github.com/X4BNet/lists_cloudflare.git", `
+    "https://github.com/X4BNet/lists_route53.git"
+
+foreach ($repourl in $repourls) {
+    $repo = Write-Output $repourl | ForEach-Object { $_ -replace "^.*/" } | ForEach-Object { $_ -replace "\.git$" }
+    if ( Test-Path -Path $repo ) {
+        Set-Location $repo
+        $result = git pull 2>&1
+        Write-Output "${repourl}: $result"
+        Set-Location ..
+    } else {
+        $result = git clone $repourl 2>&1
+        Write-Output "${repourl}: $result"
+    }
+}
+
+Set-Location "$currentDirectory"
