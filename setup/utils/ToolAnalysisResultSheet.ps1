1c90e7e36f8639918046c3e22d2979cf9909a7fa
Coding for fun <peter@reuteras.net>
2023-12-29 11:58:36 +0100

    Add script to start web server showing the repo

---
+++ setup/utils/ToolAnalysisResultSheet.ps1
@@ -0,0 +1,2 @@
+& "C:\venv\default\Scripts\python.exe" -m http.server -d C:\git\ToolAnalysisResultSheet 8001 &
+& "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" --profile-directory=Default http://localhost:8001/
