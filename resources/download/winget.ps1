7d50263f5fd942f57eda7a1d804a9e01c52aebce
Coding for fun <peter@reuteras.net>
2024-01-13 08:31:07 +0100

    rename winget-download.ps1 to winget.ps1 and add --disable-interactivity to winget download commands

--- resources/download/winget.ps1
+++ resources/download/winget.ps1
@@ -10,21 +10,21 @@ function Clear-Tmp {
 # Autopsy - available for installation via dfirws-install.ps1
 Clear-Tmp
 Write-SynchronizedLog "winget: Downloading Autopsy."
-winget download SleuthKit.Autopsy -d .\tmp\winget 2>&1 | Out-Null
+winget download --disable-interactivity	SleuthKit.Autopsy -d .\tmp\winget 2>&1 | Out-Null
 copy-item .\tmp\winget\Autopsy*.msi .\downloads\autopsy.msi
 Clear-Tmp
 
 # DotNet 6 runtime - installed during startup
 Clear-Tmp
 Write-SynchronizedLog "winget: Downloading DotNet 6 runtime."
-winget download Microsoft.DotNet.Runtime.6 -d .\tmp\winget 2>&1 | Out-Null
+winget download --disable-interactivity Microsoft.DotNet.Runtime.6 -d .\tmp\winget 2>&1 | Out-Null
 Copy-Item .\tmp\winget\Microsoft*.exe .\downloads\dotnet6.exe
 Clear-Tmp
 
 # GoLang - available for installation via dfirws-install.ps1
 Clear-Tmp
 Write-SynchronizedLog "winget: Downloading GoLang."
-winget download Golang.Go -d .\tmp\winget 2>&1 | Out-Null
+winget download --disable-interactivity	Golang.Go -d .\tmp\winget 2>&1 | Out-Null
 Copy-Item .\tmp\winget\Go*.msi .\downloads\golang.msi
 Clear-Tmp
 
@@ -38,42 +38,42 @@ Clear-Tmp
 # Obsidian - available for installation via dfirws-install.ps1
 Clear-Tmp
 Write-SynchronizedLog "winget: Downloading Obsidian."
-winget download Obsidian.Obsidian -d .\tmp\winget 2>&1 | Out-Null
+winget download --disable-interactivity	Obsidian.Obsidian -d .\tmp\winget 2>&1 | Out-Null
 Copy-Item .\tmp\winget\Obsidian*.exe .\downloads\obsidian.exe
 Clear-Tmp
 
 # PowerShell 7 - installed during startup
 Clear-Tmp
 Write-SynchronizedLog "winget: Downloading PowerShell 7."
-winget download Microsoft.PowerShell -d .\tmp\winget 2>&1 | Out-Null
+winget download --disable-interactivity	Microsoft.PowerShell -d .\tmp\winget 2>&1 | Out-Null
 Copy-Item .\tmp\winget\PowerShell*.msi .\downloads\powershell.msi
 Clear-Tmp
 
 # Qemu - available for installation via dfirws-install.ps1
 Clear-Tmp
 Write-SynchronizedLog "winget: Downloading Qemu."
-winget download SoftwareFreedomConservancy.QEMU -d .\tmp\winget 2>&1 | Out-Null
+winget download --disable-interactivity	SoftwareFreedomConservancy.QEMU -d .\tmp\winget 2>&1 | Out-Null
 Copy-Item .\tmp\winget\QEMU*.exe .\downloads\qemu.exe
 Clear-Tmp
 
 # Ruby - available for installation via dfirws-install.ps1
 Clear-Tmp
 Write-SynchronizedLog "winget: Downloading Ruby."
-winget download RubyInstallerTeam.Ruby.3.2 -d .\tmp\winget 2>&1 | Out-Null
+winget download --disable-interactivity	RubyInstallerTeam.Ruby.3.2 -d .\tmp\winget 2>&1 | Out-Null
 Copy-Item .\tmp\winget\Ruby*.exe .\downloads\ruby.exe
 Clear-Tmp
 
 # Rust - available for installation via dfirws-install.ps1
 Clear-Tmp
 Write-SynchronizedLog "winget: Downloading Rust."
-winget download Rustlang.Rust.GNU -d .\tmp\winget 2>&1 | Out-Null
+winget download --disable-interactivity	Rustlang.Rust.GNU -d .\tmp\winget 2>&1 | Out-Null
 Copy-Item .\tmp\winget\Rust*.msi .\downloads\rust.msi
 Clear-Tmp
 
 # VirusTotal CLI
 Clear-Tmp
 Write-SynchronizedLog "winget: Downloading VirusTotal CLI."
-winget download VirusTotal.vt-cli -d .\tmp\winget 2>&1 | Out-Null
+winget download --disable-interactivity	VirusTotal.vt-cli -d .\tmp\winget 2>&1 | Out-Null
 Copy-Item .\tmp\winget\vt-cli*.zip .\downloads\vt.zip
 & "$env:ProgramFiles\7-Zip\7z.exe" x -aoa "$SETUP_PATH\vt.zip" -o"$TOOLS\bin" | Out-Null
 Clear-Tmp
