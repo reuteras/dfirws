7ce0935facb0922708b8202df3a8598f77fd9a82
Coding for fun <peter@reuteras.net>
2024-01-16 08:08:45 +0100

    Fix path for sqlite-tools-win-x64-3450000.zip

--- resources/download/http.ps1
+++ resources/download/http.ps1
@@ -237,7 +237,7 @@ Get-FileFromUri -uri "https://npcap.com/dist/npcap-1.78.exe" -FilePath ".\downlo
 Get-FileFromUri -uri "https://1.eu.dl.wireshark.org/win64/Wireshark-4.2.2-x64.exe" -FilePath ".\downloads\wireshark.exe"
 
 # https://www.sqlite.org/download.html - SQLite
-Get-FileFromUri -uri "https://sqlite.org/2023/sqlite-tools-win-x64-3450000.zip" -FilePath ".\downloads\sqlite.zip"
+Get-FileFromUri -uri "https://sqlite.org/2024/sqlite-tools-win-x64-3450000.zip" -FilePath ".\downloads\sqlite.zip"
 & "$env:ProgramFiles\7-Zip\7z.exe" x -aoa "$SETUP_PATH\sqlite.zip" -o"$TOOLS\sqlite" | Out-Null
 if (Test-Path -Path $TOOLS\sqlite) {
     Remove-Item -Recurse -Force $TOOLS\sqlite | Out-Null 2>&1
