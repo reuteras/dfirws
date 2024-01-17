a984b8bdaf1232353995a200a07449ee4e5b58a9
Coding for fun <peter@reuteras.net>
2024-01-17 12:57:02 +0100

    Switch to rclone from xcopy

--- resources/download/zimmerman.ps1
+++ resources/download/zimmerman.ps1
@@ -3,4 +3,4 @@
 # Download and update Zimmerman tools. Copy to tools folder afterwards.
 .\resources\download\Get-ZimmermanTools.ps1 -Dest .\downloads\Zimmerman >> .\log\log.txt 2>&1
 
-xcopy /E $SETUP_PATH\Zimmerman $TOOLS\Zimmerman | Out-Null
+rclone.exe sync --verbose --checksum $SETUP_PATH\Zimmerman $TOOLS\Zimmerman 2>&1 | Out-Null
