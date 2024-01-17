98f468fe6b5edb80d82c1197a1c92af6b7675a79
Coding for fun <peter@reuteras.net>
2024-01-17 13:02:20 +0100

    Add output to install commands

--- setup/wscommon.ps1
+++ setup/wscommon.ps1
@@ -103,16 +103,19 @@ function Write-DateLog {
 
 # Functions to help install programs
 function Install-Apimonitor {
+    Write-Output "Installing API Monitor"
     Copy-Item "$SETUP_PATH\apimonitor64.exe" "$TEMP"
     & $TEMP\apimonitor64.exe /s /v/qn
     Add-ToUserPath "${env:ProgramFiles(x86)}\rohitab.com\API Monitor"
 }
 
 function Install-Autopsy {
+    Write-Output "Installing Autopsy"
     msiexec.exe /i "$SETUP_PATH\autopsy.msi" /qn /norestart
 }
 
 function Install-BashExtra {
+    Write-Output "Installing Bash extras"
     Set-Location "$env:ProgramFiles\Git"
     Get-ChildItem -Path $SETUP_PATH\bash -Include "*.tar" -Recurse |
         ForEach-Object {
@@ -122,10 +125,12 @@ function Install-BashExtra {
 }
 
 function Install-Choco {
+    Write-Output "Installing Chocolatey"
     & "$SETUP_PATH\choco\tools\chocolateyInstall.ps1"
 }
 
 function Install-CMDer {
+    Write-Output "Installing Cmder"
     & "$env:ProgramFiles\7-Zip\7z.exe" x -aoa "$SETUP_PATH\cmder.7z" -o"$env:ProgramFiles\cmder"
     Add-ToUserPath "$env:ProgramFiles\cmder"
     Add-ToUserPath "$env:ProgramFiles\cmder\bin"
@@ -134,41 +139,49 @@ function Install-CMDer {
 }
 
 function Install-GitBash {
+    Write-Output "Installing Git Bash"
     Copy-Item "$SETUP_PATH\git.exe" "$TEMP\git.exe"
     Copy-Item "$HOME\Documents\tools\.bashrc" "$HOME\"
     & "$TEMP\git.exe" /VERYSILENT /NORESTART /NOCANCEL /SP- /CLOSEAPPLICATIONS /RESTARTAPPLICATIONS /COMPONENTS="icons,ext\reg\shellhere,assoc,assoc_sh"
 }
 
 function Install-GoLang {
+    Write-Output "Installing GoLang"
     Copy-Item "$SETUP_PATH\golang.msi" "$TEMP\golang.msi"
     Start-Process -Wait msiexec -ArgumentList "/i $TEMP\golang.msi /qn /norestart"
     Add-ToUserPath "$env:ProgramFiles\Go\bin"
 }
 
 function Install-Hashcat {
+    Write-Output "Installing Hashcat"
     Copy-Item -Recurse "$TOOLS\hashcat" "$env:ProgramFiles"
 }
 
 function Install-Jadx {
+    Write-Output "Installing Jadx"
     & "$env:ProgramFiles\7-Zip\7z.exe" x -aoa "$SETUP_PATH\jadx.zip" -o"$env:ProgramFiles\jadx"
 }
 
 function Install-Kape {
+    Write-Output "Installing Kape"
     Copy-Item -Recurse "$SETUP_PATH\KAPE" "$env:ProgramFiles"
     Add-Shortcut -SourceLnk "$HOME\Desktop\dfirws\IR\gkape.lnk" -DestinationPath "$env:ProgramFiles\KAPE\gkape.exe" -WorkingDirectory "$HOME\Desktop" -Iconlocation "$env:ProgramFiles\KAPE\gkape.exe"
     Add-Shortcut -SourceLnk "$HOME\Desktop\dfirws\IR\kape.lnk" -DestinationPath "$POWERSHELL_EXE" -WorkingDirectory "$HOME\Desktop"
 }
 
 function Install-LibreOffice {
+    Write-Output "Installing LibreOffice"
     Copy-Item "$SETUP_PATH\LibreOffice.msi" "$TEMP\LibreOffice.msi"
     Start-Process -Wait msiexec -ArgumentList "/qb /i $TEMP\LibreOffice.msi /l* $TEMP\LibreOffice_install_log.txt REGISTER_ALL_MSO_TYPES=1 UI_LANGS=en_US ISCHECKFORPRODUCTUPDATES=0 REBOOTYESNO=No QUICKSTART=0 ADDLOCAL=ALL VC_REDIST=0 REMOVE=gm_o_Onlineupdate,gm_r_ex_Dictionary_Af,gm_r_ex_Dictionary_An,gm_r_ex_Dictionary_Ar,gm_r_ex_Dictionary_Be,gm_r_ex_Dictionary_Bg,gm_r_ex_Dictionary_Bn,gm_r_ex_Dictionary_Bo,gm_r_ex_Dictionary_Br,gm_r_ex_Dictionary_Pt_Br,gm_r_ex_Dictionary_Bs,gm_r_ex_Dictionary_Pt_Pt,gm_r_ex_Dictionary_Ca,gm_r_ex_Dictionary_Cs,gm_r_ex_Dictionary_Da,gm_r_ex_Dictionary_Nl,gm_r_ex_Dictionary_Et,gm_r_ex_Dictionary_Gd,gm_r_ex_Dictionary_Gl,gm_r_ex_Dictionary_Gu,gm_r_ex_Dictionary_He,gm_r_ex_Dictionary_Hi,gm_r_ex_Dictionary_Hu,gm_r_ex_Dictionary_Lt,gm_r_ex_Dictionary_Lv,gm_r_ex_Dictionary_Ne,gm_r_ex_Dictionary_No,gm_r_ex_Dictionary_Oc,gm_r_ex_Dictionary_Pl,gm_r_ex_Dictionary_Ro,gm_r_ex_Dictionary_Ru,gm_r_ex_Dictionary_Si,gm_r_ex_Dictionary_Sk,gm_r_ex_Dictionary_Sl,gm_r_ex_Dictionary_El,gm_r_ex_Dictionary_Es,gm_r_ex_Dictionary_Te,gm_r_ex_Dictionary_Th,gm_r_ex_Dictionary_Tr,gm_r_ex_Dictionary_Uk,gm_r_ex_Dictionary_Vi,gm_r_ex_Dictionary_Zu,gm_r_ex_Dictionary_Sq,gm_r_ex_Dictionary_Hr,gm_r_ex_Dictionary_De,gm_r_ex_Dictionary_Id,gm_r_ex_Dictionary_Is,gm_r_ex_Dictionary_Ko,gm_r_ex_Dictionary_Lo,gm_r_ex_Dictionary_Mn,gm_r_ex_Dictionary_Sr,gm_r_ex_Dictionary_Eo,gm_r_ex_Dictionary_It,gm_r_ex_Dictionary_Fr"
 }
 
 function Install-Loki {
+    Write-Output "Installing Loki"
     & "$env:ProgramFiles\7-Zip\7z.exe" x -aoa "$SETUP_PATH\loki.zip" -o"$env:ProgramFiles\"
     Copy-Item $GIT\signature-base "$env:ProgramFiles\loki" -Recurse
 }
 function Install-Neo4j {
+    Write-Output "Installing Neo4j"
     Copy-Item "$SETUP_PATH\microsoft-jdk-11.msi" "$TEMP\microsoft-jdk-11.msi"
     Start-Process -Wait msiexec -ArgumentList "/i $TEMP\microsoft-jdk-11.msi ADDLOCAL=FeatureMain,FeatureEnvironment,FeatureJarFileRunWith,FeatureJavaHome INSTALLDIR=$NEO_JAVA /qn /norestart"
     & "$env:ProgramFiles\7-Zip\7z.exe" x -aoa "$SETUP_PATH\neo4j.zip" -o"$env:ProgramFiles"
@@ -179,35 +192,42 @@ function Install-Neo4j {
 }
 
 function Install-Node {
+    Write-Output "Installing Node"
     Copy-Item -r "$TOOLS\node" $HOME\Desktop
 }
 
 function Install-Obsidian {
+    Write-Output "Installing Obsidian"
     Copy-Item "$SETUP_PATH\obsidian.exe" "$TEMP\obsidian.exe"
     Start-Process -Wait "$TEMP\obsidian.exe" -ArgumentList '/S /V"/qn REBOOT=ReallySuppress"'
 }
 
 function Install-PDFStreamDumper {
+    Write-Output "Installing PDFStreamDumper"
     Copy-Item "$SETUP_PATH\PDFStreamDumper.exe" "$TEMP\PDFStreamDumper.exe"
     & "$TEMP\PDFStreamDumper.exe" /verysilent
 }
 
 function Install-Qemu {
+    Write-Output "Installing Qemu"
     Copy-Item "$SETUP_PATH\qemu.exe" "$TEMP\qemu.exe"
     Start-Process -Wait "$TEMP\qemu.exe" -ArgumentList '/S /V"/qn REBOOT=ReallySuppress"'
 }
 
 function  Install-Ruby {
+    Write-Output "Installing Ruby"
     & "$SETUP_PATH\ruby.exe" /verysilent /allusers /dir="$env:ProgramFiles\ruby"
     Add-ToUserPath "$env:ProgramFiles\ruby\bin"
 }
 
 function Install-Rust {
+    Write-Output "Installing Rust"
     Copy-Item "$SETUP_PATH\rust.msi" "$TEMP\rust.msi"
     Start-Process -Wait msiexec -ArgumentList "/i $TEMP\rust.msi INSTALLDIR=C:\Rust /qn /norestart"
 }
 
 function Install-VSCode {
+    Write-Output "Installing Visual Studio Code"
     . $TEMP\default-config.ps1
     . $TEMP\config.ps1
     Copy-Item "$SETUP_PATH\vscode.exe" "$TEMP\vscode.exe"
@@ -227,6 +247,7 @@ function Install-VSCode {
 }
 
 function Install-W10Loopback {
+    Write-Output "Installing Windows 10 Loopback adapter"
     $WIN10=(Get-ComputerInfo | Select-Object -expand OsName) -match 10
     if ($WIN10) {
        & "$HOME\Documents\tools\utils\devcon.exe" install $env:windir\inf\netloop.inf *msloop
@@ -237,11 +258,13 @@ function Install-W10Loopback {
 }
 
 function Install-X64dbg {
+    Write-Output "Installing x64dbg"
     & "$env:ProgramFiles\7-Zip\7z.exe" x -aoa "$SETUP_PATH\x64dbg.zip" -o"$env:ProgramFiles\x64dbg"
     Add-ToUserPath "$env:ProgramFiles\x64dbg\release\x32"
     Add-ToUserPath "$env:ProgramFiles\x64dbg\release\x64"
 }
 function Install-Zui {
+    Write-Output "Installing Zui"
     Copy-Item "$SETUP_PATH\zui.exe" "$TEMP\zui.exe"
     & "$TEMP\zui.exe" /S /AllUsers
 }
