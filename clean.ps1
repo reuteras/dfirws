69dce4c347f66000bc5b19ce3a9ac049e57b5d7e
Coding for fun <peter@reuteras.net>
2024-01-18 09:28:16 +0100

    Also remove tools_downloaded.csv when cleaning up

--- clean.ps1
+++ clean.ps1
@@ -3,3 +3,4 @@ Remove-Item -Recurse -Force downloads > $null 2>&1
 Remove-Item -Recurse -Force log > $null 2>&1
 Remove-Item -Recurse -Force mount > $null 2>&1
 Remove-Item -Recurse -Force tmp > $null 2>&1
+Remove-Item -Force tools_downloaded.csv > $null 2>&1
