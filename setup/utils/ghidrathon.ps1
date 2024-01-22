a650ad2d9148527272d3f45eeabd3f828fb02cb4
Coding for fun <peter@reuteras.net>
2024-01-22 11:51:30 +0100

    Update to Ghidra 11.0

--- setup/utils/ghidrathon.ps1
+++ setup/utils/ghidrathon.ps1
@@ -10,8 +10,8 @@ deactivate
 & "${VENV}\jep\Scripts\Activate.ps1"
 $env:PYTHONPATH="${VENV}\jep\Lib\site-packages;C:\Program Files\Python311\Lib"
 
-if (!(Test-Path -Path "${HOME}\.ghidra\.ghidra_10.4_PUBLIC\Extensions\ghidrathon")) {
-    & "$env:programfiles\7-Zip\7z.exe" x -o"${HOME}\.ghidra\.ghidra_10.4_PUBLIC\Extensions" "$TOOLS\ghidra_extensions\ghidrathon.zip"
+if (!(Test-Path -Path "${HOME}\.ghidra\.ghidra_11.0_PUBLIC\Extensions\ghidrathon")) {
+    & "$env:programfiles\7-Zip\7z.exe" x -o"${HOME}\.ghidra\.ghidra_11.0_PUBLIC\Extensions" "$TOOLS\ghidra_extensions\ghidrathon.zip"
 }
 
-& "${TOOLS}\ghidra\ghidra_10.4_PUBLIC\ghidraRun.bat"
+& "${TOOLS}\ghidra\ghidra_11.0_PUBLIC\ghidraRun.bat"
