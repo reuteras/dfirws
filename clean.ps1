33463f6cde0f465465b309aa492e349e64faa9f0
Coding for fun <peter@reuteras.net>
2024-01-23 14:59:10 +0100

    Cleanup

--- clean.ps1
+++ clean.ps1
@@ -1,6 +1,6 @@
 # Script to clean up directories created by this tool.
-Remove-Item -Recurse -Force downloads > $null 2>&1
-Remove-Item -Recurse -Force log > $null 2>&1
-Remove-Item -Recurse -Force mount > $null 2>&1
-Remove-Item -Recurse -Force tmp > $null 2>&1
-Remove-Item -Force tools_downloaded.csv > $null 2>&1
+Remove-Item -Recurse -Force "downloads" 2>&1 | Out-Null
+Remove-Item -Recurse -Force "log" 2>&1 | Out-Null
+Remove-Item -Recurse -Force "mount" 2>&1 | Out-Null
+Remove-Item -Recurse -Force "tmp" 2>&1 | Out-Null
+Remove-Item -Force tools_"downloaded.csv" 2>&1 | Out-Null
