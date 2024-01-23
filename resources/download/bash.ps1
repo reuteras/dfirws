dd7d77942c2f0ed609603cf5f52f1cbb6ad3bd73
Coding for fun <peter@reuteras.net>
2024-01-23 15:02:11 +0100

    Cleanup

--- resources/download/bash.ps1
+++ resources/download/bash.ps1
@@ -5,22 +5,22 @@ param (
 $ScriptRoot = "$ScriptRoot\resources\download"
 $ROOT_PATH = Resolve-Path "$ScriptRoot\..\..\"
 
-. $ScriptRoot\common.ps1
+. "${ScriptRoot}\common.ps1"
 
-Write-DateLog "Download packages for Git for Windows (bash)." > $ROOT_PATH\log\bash.txt
-Write-DateLog "" >> $ROOT_PATH\log\bash.txt
+Write-DateLog "Download packages for Git for Windows (bash)." > "${ROOT_PATH}\log\bash.txt"
+Write-DateLog "" >> "${ROOT_PATH}\log\bash.txt"
 
 # Check current files
-if (Test-Path -Path "$ROOT_PATH\tools_downloaded.csv" ) {
-    $StartHASH = Get-FileHash -Path "$ROOT_PATH\tools_downloaded.csv" -Algorithm SHA256 | Select-Object -ExpandProperty Hash
+if (Test-Path -Path "${ROOT_PATH}\tools_downloaded.csv" ) {
+    ${StartHASH} = Get-FileHash -Path "${ROOT_PATH}\tools_downloaded.csv" -Algorithm SHA256 | Select-Object -ExpandProperty Hash
 } else {
-    $StartHASH = ""
+    ${StartHASH} = ""
 }
 
 $mutex = New-Object System.Threading.Mutex($false, $mutexName)
 
-if (Test-Path -Path $ROOT_PATH\tmp\downloads\bash ) {
-    Remove-Item -r -Force $ROOT_PATH\tmp\downloads\bash
+if (Test-Path -Path "${ROOT_PATH}\tmp\downloads\bash" ) {
+    Remove-Item -r -Force "${ROOT_PATH}\tmp\downloads\bash"
 }
 
 $packages = "bash-completion", `
@@ -40,21 +40,21 @@ $packages = "bash-completion", `
     "zstd"
 
 foreach ($package in $packages) {
-    $url = Get-DownloadUrlMSYS "$package"
-    Get-FileFromUri -uri "$url" -FilePath ".\downloads\bash\$package.pkg.tar.zst" -CheckURL "Yes"
+    $url = Get-DownloadUrlMSYS "${package}"
+    Get-FileFromUri -uri "${url}" -FilePath ".\downloads\bash\${package}.pkg.tar.zst" -CheckURL "Yes"
 }
 
-$FinalHASH = Get-FileHash -Path "$ROOT_PATH\tools_downloaded.csv" -Algorithm SHA256 | Select-Object -ExpandProperty Hash
+$FinalHASH = Get-FileHash -Path "${ROOT_PATH}\tools_downloaded.csv" -Algorithm SHA256 | Select-Object -ExpandProperty Hash
 
 if (Compare-Object $StartHASH $FinalHASH) {
-    (Get-Content $ROOT_PATH\resources\templates\generate_bash.wsb.template).replace('__SANDBOX__', $ROOT_PATH) | Set-Content $ROOT_PATH\tmp\generate_bash.wsb
+    (Get-Content ${ROOT_PATH}\resources\templates\generate_bash.wsb.template).replace('__SANDBOX__', "${ROOT_PATH}") | Set-Content "${ROOT_PATH}\tmp\generate_bash.wsb"
 
     $mutex.WaitOne() | Out-Null
-    & $ROOT_PATH\tmp\generate_bash.wsb
+    & "${ROOT_PATH}\tmp\generate_bash.wsb"
     Start-Sleep 10
-    Remove-Item $ROOT_PATH\tmp\generate_bash.wsb
+    Remove-Item "${ROOT_PATH}\tmp\generate_bash.wsb"
 
-    Stop-SandboxWhenDone "$ROOT_PATH\downloads\bash\done" $mutex
+    Stop-SandboxWhenDone "${ROOT_PATH}\downloads\bash\done" ${mutex}
 }
 
-Write-DateLog "Git for Windows (bash) done." >> $ROOT_PATH\log\bash.txt
+Write-DateLog "Git for Windows (bash) done." >> "${ROOT_PATH}\log\bash.txt"
