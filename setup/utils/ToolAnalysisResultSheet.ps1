4437d284be946f89ea3c914d760876e3b9dfb1d2
Coding for fun <peter@reuteras.net>
2024-01-30 10:42:57 +0100

    Use Start-Process to start the python server in the background

--- setup/utils/ToolAnalysisResultSheet.ps1
+++ setup/utils/ToolAnalysisResultSheet.ps1
@@ -1,2 +1,2 @@
-& "C:\venv\default\Scripts\python.exe" -m http.server -d C:\git\ToolAnalysisResultSheet 8001 &
-& "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" --profile-directory=Default http://localhost:8001/
+Start-Process -FilePath "C:\venv\default\Scripts\python.exe" -ArgumentList "-m http.server -d C:\git\ToolAnalysisResultSheet 8001"
+& "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" --profile-directory=Default http://127.0.0.1:8001/
