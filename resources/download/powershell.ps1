e4a49134e305ff17e3ee72756f1e61dfc9e0eec3
Coding for fun <peter@reuteras.net>
2024-01-31 12:01:07 +0100

    Add script to download powershell modules

---
+++ resources/download/powershell.ps1
@@ -0,0 +1,16 @@
+. "${PSScriptRoot}\common.ps1"
+
+Clear-Tmp powershell
+
+# Terminal-Icons
+Write-SynchronizedLog "powershell: Downloading Terminal-Icons."
+Save-Module -Name Terminal-Icons -Path .\tmp\powershell -Force
+
+# posh-git
+Write-SynchronizedLog "powershell: Downloading posh-git."
+Save-Module -Name posh-git -Path .\tmp\powershell -Force
+
+rclone.exe sync --verbose --checksum ".\tmp\powershell" ".\downloads\powershell-modules" 2>&1 | Out-Null
+Clear-Tmp powershell
+
+Write-SynchronizedLog "powershell: Download complete."
