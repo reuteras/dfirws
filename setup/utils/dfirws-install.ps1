0e59f2c75a6e9da03c7c2837e4c75da05ed4102b
Coding for fun <peter@reuteras.net>
2024-01-31 11:58:33 +0100

    Add OhMyPosh to dfirws-install.ps1 and fix .IsPresent checks

--- setup/utils/dfirws-install.ps1
+++ setup/utils/dfirws-install.ps1
@@ -16,6 +16,7 @@ param (
     [switch]$Neo4j,
     [switch]$Node,
     [switch]$Obsidian,
+    [switch]$OhMyPosh,
     [switch]$PDFStreamDumper,
     [switch]$Qemu,
     [switch]$Ruby,
@@ -73,50 +74,54 @@ if ($LibreOffice.IsPresent) {
     Install-LibreOffice
 }
 
-if ($Loki) {
+if ($Loki.IsPresent) {
     Install-Loki
 }
 
-if ($Neo4j) {
+if ($Neo4j.IsPresent) {
     Install-Neo4j
 }
 
-if ($Node) {
+if ($Node.IsPresent) {
     Install-Node
 }
 
-if ($Obsidian) {
+if ($Obsidian.IsPresent) {
     Install-Obsidian
 }
 
-if ($PDFStreamDumper) {
+if ($OhMyPosh.IsPresent) {
+    Install-OhMyPosh
+}
+
+if ($PDFStreamDumper.IsPresent) {
     Install-PDFStreamDumper
 }
 
-if ($Qemu) {
+if ($Qemu.IsPresent) {
     Install-Qemu
 }
 
-if ($Ruby) {
+if ($Ruby.IsPresent) {
     Install-Ruby
 }
 
-if ($Rust) {
+if ($Rust.IsPresent) {
     Install-Rust
 }
 
-if ($VSCode) {
+if ($VSCode.IsPresent) {
     Install-VSCode
 }
 
-if ($Wireshark) {
+if ($Wireshark.IsPresent) {
     Install-Wireshark
 }
 
-if ($X64dbg) {
+if ($X64dbg.IsPresent) {
     Install-X64dbg
 }
 
-if ($Zui) {
+if ($Zui.IsPresent) {
     Install-Zui
 }
