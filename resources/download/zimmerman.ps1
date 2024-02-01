3c66723926b111312ebd55828d929b11a0f23827
Coding for fun <peter@reuteras.net>
2024-02-01 09:46:17 +0100

    Install ZimmermanTools to Tools directory directly

--- resources/download/zimmerman.ps1
+++ resources/download/zimmerman.ps1
@@ -1,4 +1,4 @@
 . "${PSScriptRoot}\common.ps1"
 
-# Download and update Zimmerman tools. Copy to tools folder afterwards.
-.\resources\download\Get-ZimmermanTools.ps1 -Dest ".\downloads\Zimmerman" 2>&1 >> ".\log\log.txt"
+# Download and update Zimmerman tools.
+.\resources\download\Get-ZimmermanTools.ps1 -Dest ".\mount\Tools\Zimmerman" 2>&1 >> ".\log\log.txt"
