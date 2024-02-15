c26ff6a030144172afa307ad1c2bfcd9ab2dc763
Coding for fun <peter@reuteras.net>
2024-02-15 12:54:06 +0100

    Update sync

--- resources/contrib/sync/sync_from_usb.ps1
+++ resources/contrib/sync/sync_from_usb.ps1
@@ -1,2 +1,8 @@
 Robocopy.exe E:\ M:\IT\dfirws\ dfirws.zip
-Robocopy.exe E:\dfirws\ M:\IT\dfirws\dfirws\ /MIR /XF "update_and_run.ps1" /XF "config.txt" /XF "dfirws.ps1" /MT:96
+Robocopy.exe E:\dfirws\downloads M:\IT\dfirws\dfirws\downloads /MIR /MT:96
+Robocopy.exe E:\dfirws\enrichment M:\IT\dfirws\dfirws\enrichment /MIR /MT:96
+Robocopy.exe E:\dfirws\local\defaults M:\IT\dfirws\dfirws\local\defaults /MIR /MT:96
+Robocopy.exe E:\dfirws\mount M:\IT\dfirws\dfirws\mount /MIR /MT:96
+Robocopy.exe E:\dfirws\setup M:\IT\dfirws\dfirws\setup /MIR /MT:96
+Robocopy.exe E:\dfirws\ M:\IT\dfirws\dfirws\ createSandboxConfig.ps1
+Robocopy.exe E:\dfirws\ M:\IT\dfirws\dfirws\ README.md
