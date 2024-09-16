7b5c6c605fbeb38a7cbcf766932173734a34206d
Coding for fun <peter@reuteras.net>
2024-09-16 10:57:44 +0200

    Fix path for msys2

--- resources/download/msys2.ps1
+++ resources/download/msys2.ps1
@@ -19,8 +19,8 @@ Remove-Item -Recurse -Force "${ROOT_PATH}\tmp\msys64\msys2.txt" 2>&1 | Out-Null
 
 $mutex = New-Object System.Threading.Mutex($false, $mutexName)
 
-if (! (Test-Path -Path "${ROOT_PATH}\mount\msys64" )) {
-    New-Item -ItemType Directory -Force -Path "${ROOT_PATH}\mount\msys64" | Out-Null
+if (! (Test-Path -Path "${ROOT_PATH}\mount\Tools\msys64" )) {
+    New-Item -ItemType Directory -Force -Path "${ROOT_PATH}\mount\Tools\msys64" | Out-Null
 }
 
 (Get-Content ${ROOT_PATH}\resources\templates\generate_msys2.wsb.template).replace('__SANDBOX__', "${ROOT_PATH}") | Set-Content "${ROOT_PATH}\tmp\generate_msys2.wsb"
@@ -32,7 +32,7 @@ Remove-Item "${ROOT_PATH}\tmp\generate_msys2.wsb" | Out-Null
 
 Stop-SandboxWhenDone "${ROOT_PATH}\tmp\msys64\done" $mutex | Out-Null
 
-rclone.exe sync --verbose --checksum "${ROOT_PATH}\tmp\msys64" "${ROOT_PATH}\mount\msys64"
+rclone.exe sync --verbose --checksum "${ROOT_PATH}\tmp\msys64" "${ROOT_PATH}\mount\Tools\msys64"
 Write-DateLog "MSYS2 and packages done." >> ${ROOT_PATH}\log\msys2.txt 2>&1
 
 Remove-Item -Recurse -Force "${ROOT_PATH}\tmp\msys64" 2>&1 | Out-Null
