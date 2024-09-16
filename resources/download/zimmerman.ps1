3115b6548b1fba4884de270bbe21ad74891b760a
Coding for fun <peter@reuteras.net>
2024-09-16 10:51:05 +0200

    Call with PowerShell

--- resources/download/zimmerman.ps1
+++ resources/download/zimmerman.ps1
@@ -1,4 +1,4 @@
 . "${PSScriptRoot}\common.ps1"
 
 # Download and update Zimmerman tools.
-.\resources\download\Get-ZimmermanTools.ps1 -Dest ".\mount\Tools\Zimmerman" 2>&1 >> ".\log\log.txt"
+PowerShell.exe -NoProfile -ExecutionPolicy Bypass -File .\resources\download\Get-ZimmermanTools.ps1 -Dest ".\mount\Tools\Zimmerman" 2>&1 >> ".\log\log.txt"
