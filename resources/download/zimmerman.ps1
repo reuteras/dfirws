a234c50312a120e4ad1314c2db279f69a2862522
Peter Reuterås <peter@reuteras.net>
2025-07-02 09:51:29 +0200

    Run Sync-EZTools.ps1 to update the tools and plugins.

--- resources/download/zimmerman.ps1
+++ resources/download/zimmerman.ps1
@@ -2,3 +2,4 @@
 
 # Download and update Zimmerman tools.
 PowerShell.exe -NoProfile -ExecutionPolicy Bypass -File .\resources\download\Get-ZimmermanTools.ps1 -Dest ".\mount\Tools\Zimmerman" 2>&1 >> ".\log\log.txt"
+PowerShell.exe -NoProfile -ExecutionPolicy Bypass -File .\resources\download\Sync-EZTools.ps1 ".\mount\Tools\Zimmerman" 2>&1 >> ".\log\log.txt"
