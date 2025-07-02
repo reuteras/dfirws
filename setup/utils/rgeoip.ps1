bf46c86f024d82cd0a25cfa2f3785dc5a2cf95af
Peter Reuterås <peter@reuteras.net>
2025-07-02 07:53:30 +0200

    Remove full path

--- setup/utils/rgeoip.ps1
+++ setup/utils/rgeoip.ps1
@@ -4,4 +4,4 @@ if (-not (Test-Path -Path "C:\enrichment\maxmind_current\GeoLite2-City.mmdb")) {
     return
 }
 
-$input | C:\venv\dissect\Scripts\rgeoip.exe -c "C:\enrichment\maxmind_current\GeoLite2-City.mmdb" -a "C:\enrichment\maxmind_current\GeoLite2-ASN.mmdb" $args
+$input | rgeoip.exe -c "C:\enrichment\maxmind_current\GeoLite2-City.mmdb" -a "C:\enrichment\maxmind_current\GeoLite2-ASN.mmdb" $args
