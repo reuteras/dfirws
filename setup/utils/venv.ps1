3431308ae3c3436457eda90c4a75d79b4c148ccd
Peter Reuterås <peter@reuteras.net>
2025-07-17 06:32:14 +0200

    Remove old venvs no longer in use

--- setup/utils/venv.ps1
+++ setup/utils/venv.ps1
@@ -1,52 +1,24 @@
 # Script to switch between virtual environments
 
 param(
-    [Parameter(HelpMessage = "Switch to venv aspose.")]
-    [Switch]$aspose,
-    [Parameter(HelpMessage = "Switch to venv binary-refinery.")]
-    [Switch]$binaryrefinery,
-    [Parameter(HelpMessage = "Switch to venv chepy.")]
-    [Switch]$chepy,
-    [Parameter(HelpMessage = "Switch to venv csvkit.")]
-    [Switch]$csvkit,
     [Parameter(HelpMessage = "Switch to venv default.")]
     [Switch]$default,
     [Parameter(HelpMessage = "Switch to venv dfir-unfurl.")]
     [Switch]$unfurl,
-    [Parameter(HelpMessage = "Switch to venv dissect.")]
-    [Switch]$dissect,
     [Parameter(HelpMessage = "Switch to venv evt2sigma.")]
     [Switch]$evt2sigma,
-    [Parameter(HelpMessage = "Switch to venv flare-floss.")]
-    [Switch]$floss,
-    [Parameter(HelpMessage = "Switch to venv ghidrecomp.")]
-    [Switch]$ghidrecomp,
-    [Parameter(HelpMessage = "Switch to venv jep.")]
-    [Switch]$jep,
-    [Parameter(HelpMessage = "Switch to venv jpterm.")]
-    [Switch]$jpterm,
-    [Parameter(HelpMessage = "Switch to venv litecli.")]
-    [Switch]$litecli,
-    [Parameter(HelpMessage = "Switch to venv magika.")]
-    [Switch]$magika,
-    [Parameter(HelpMessage = "Switch to venv maldump.")]
-    [Switch]$maldump,
-    [Parameter(HelpMessage = "Switch to venv mwcp.")]
-    [Switch]$Kanvas,
     [Parameter(HelpMessage = "Switch to venv Kanvas.")]
+    [Switch]$Kanvas,
+    [Parameter(HelpMessage = "Switch to venv pe2pic.")]
     [Switch]$pe2pic,
-    [Parameter(HelpMessage = "Switch to venv peepdf3.")]
-    [Switch]$peepdf3,
-    [Parameter(HelpMessage = "Switch to venv regipy.")]
-    [Switch]$regipy,
-    [Parameter(HelpMessage = "Switch to venv rexi.")]
-    [Switch]$rexi,
     [Parameter(HelpMessage = "Switch to venv scare.")]
     [Switch]$scare,
-    [Parameter(HelpMessage = "Switch to venv sigma-cli.")]
-    [Switch]$sigma,
+    [Parameter(HelpMessage = "Switch to venv srum-dump.")]
+    [Switch]$srumdump,
     [Parameter(HelpMessage = "Switch to venv white-phoenix.")]
-    [Switch]$whitephoenix
+    [Switch]$whitephoenix,
+    [Parameter(HelpMessage = "Switch to venv zircolite.")]
+    [Switch]$zircolite
 )
 
 $venv = "default"
