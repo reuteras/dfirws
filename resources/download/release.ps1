8b573be6cb0b5e888ecff548aea247d872cdcc2b
Peter Reuterås <peter@reuteras.net>
2026-04-22 07:50:42 +0200

    Escape $

--- resources/download/release.ps1
+++ resources/download/release.ps1
@@ -3844,7 +3844,7 @@ $TOOL_DEFINITIONS += @{
     FileExtensions = @(".dmp", ".raw", ".vmem", ".img")
     Tags = @("memory-forensics", "filesystem")
     Notes = "MemProcFS is an easy and convenient way of viewing physical memory as files in a virtual file system."
-    Tips = "To fix the problem with python from Cutter you can run this in the terminal before running MemProcFS: $env:PATH = ($env:PATH -split ';' | Where-Object {$_ -notlike '*cutter*'}) -join ';'"
+    Tips = "To fix the problem with python from Cutter you can run this in the terminal before running MemProcFS: `$env:PATH = (`$env:PATH -split ';' | Where-Object {`$_ -notlike '*cutter*'}) -join ';'"
     Usage = ""
     SampleCommands = @()
     SampleFiles = @()
